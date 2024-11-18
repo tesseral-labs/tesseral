667816ab8a94ec77d1d28e601178623d3a7c350a
Blake Williams <blake.williams@ssoready.com>
2024-11-18 13:22:11 -0800

    Schema updates

---
+++ cmd/migrate/migrations/000004_add_sessions_logic.up.sql
@@ -0,0 +1,46 @@
+create type auth_method as enum ('email', 'google', 'microsoft');
+
+-- a user's email address is verified by sending a verification challenge to the email address
+-- and then having the user provide the verification token
+create table method_verification_challenges (
+  id                uuid NOT NULL,
+  -- verifications are scoped to the project to ensure that each auth method
+  -- has a unique set of verification challenges and is only verified once 
+  -- per-project
+  project_id        uuid NOT NULL references projects(id),
+  complete_time     timestamp with time zone,
+  -- the email address to which the verification challenge was sent
+  email             character varying NOT NULL,
+  -- the auth method for which the verification challenge was sent
+  auth_method       auth_method NOT NULL,
+  expire_time       timestamp with time zone NOT NULL,
+  -- the verification token that the user must provide to verify the email address
+  secret_token      character varying NOT NULL,
+);
+
+-- temprorary session created after the first step of the login process.
+-- this session has a much more limited permissions scope than the final session.
+create table intermediate_sessions(
+  id                            uuid not null primary key,
+  user_id                       uuid not null,
+  created_time                  timestamp with time zone not null default now(),
+  -- the timestamp for when the session expires
+  expire_time                   timestamp with time zone not null,
+  token                         character varying not null,
+  token_sha256                  bytea,
+  revoked                       boolean not null default false
+);
+
+-- final session created after the user has verified their email address and/or
+-- completed the login process (if verification is no longer required).
+create table sessions
+(
+  id                            uuid not null primary key,
+  user_id                       uuid not null references users(id),
+  created_time                  timestamp with time zone not null default now(),
+  -- the timestamp for when the session expires
+  expire_time                   timestamp with time zone not null,
+  token                         character varying not null,
+  token_sha256                  bytea,
+  revoked                       boolean not null default false
+);
