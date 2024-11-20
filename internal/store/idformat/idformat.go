86890f1ad8da7355105e4401211e21278a0cb586
Blake Williams <blake.williams@ssoready.com>
2024-11-20 12:01:28 -0800

    Remove openauth prefixes

--- internal/store/idformat/idformat.go
+++ internal/store/idformat/idformat.go
@@ -5,14 +5,14 @@ import "github.com/ssoready/prettyuuid"
 const alphabet = "0123456789abcdefghijklmnopqrstuvwxyz"
 
 var (
-	APIKey                					= prettyuuid.MustNewFormat("openauth_api_key_", alphabet)
+	APIKey                					= prettyuuid.MustNewFormat("api_key_", alphabet)
 	APISecretKey          					= prettyuuid.MustNewFormat("openauth_secret_key_", alphabet)
-	IntermediateSession   					= prettyuuid.MustNewFormat("openauth_intermediate_session_", alphabet)
-	IntermediateSessionSigningKey 	= prettyuuid.MustNewFormat("openauth_intermediate_session_signing_key_", alphabet)
-	MethodVerificationChallenge 		= prettyuuid.MustNewFormat("openauth_method_verification_challenge_", alphabet)
-	Organization          					= prettyuuid.MustNewFormat("openauth_org_", alphabet)
-	Project							  					= prettyuuid.MustNewFormat("openauth_project_", alphabet)
-	Session               					= prettyuuid.MustNewFormat("openauth_session_", alphabet)
-	SessionSigningKey								= prettyuuid.MustNewFormat("openauth_session_signing_key_", alphabet)
-	User                  					= prettyuuid.MustNewFormat("openauth_user_", alphabet)
+	IntermediateSession   					= prettyuuid.MustNewFormat("intermediate_session_", alphabet)
+	IntermediateSessionSigningKey 	= prettyuuid.MustNewFormat("intermediate_session_signing_key_", alphabet)
+	MethodVerificationChallenge 		= prettyuuid.MustNewFormat("method_verification_challenge_", alphabet)
+	Organization          					= prettyuuid.MustNewFormat("org_", alphabet)
+	Project							  					= prettyuuid.MustNewFormat("project_", alphabet)
+	Session               					= prettyuuid.MustNewFormat("session_", alphabet)
+	SessionSigningKey								= prettyuuid.MustNewFormat("session_signing_key_", alphabet)
+	User                  					= prettyuuid.MustNewFormat("user_", alphabet)
 )
