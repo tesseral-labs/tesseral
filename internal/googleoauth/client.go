93dca2de9b9283f4bf2d340304ce111502f91757
Ulysse Carion <ulysse.carion@ssoready.com>
2024-12-03 15:15:53 -0800

    add googleoauth client

---
+++ internal/googleoauth/client.go
@@ -0,0 +1,146 @@
+package googleoauth
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"net/url"
+	"strings"
+)
+
+type Client struct {
+	HTTPClient *http.Client
+}
+
+type GetAuthorizeURLRequest struct {
+	GoogleOAuthClientID string
+	RedirectURI         string
+	State               string
+}
+
+func (c *Client) GetAuthorizeURL(req *GetAuthorizeURLRequest) string {
+	u, err := url.Parse("https://accounts.google.com/o/oauth2/v2/auth")
+	if err != nil {
+		panic(fmt.Errorf("parse authorize base url: %w", err))
+	}
+
+	q := url.Values{}
+	q.Set("client_id", req.GoogleOAuthClientID)
+	q.Set("redirect_uri", req.RedirectURI)
+	q.Set("state", req.State)
+	q.Set("response_type", "code")
+	q.Set("scope", "https://www.googleapis.com/auth/userinfo.email")
+
+	u.RawQuery = q.Encode()
+	return u.String()
+}
+
+type RedeemCodeRequest struct {
+	GoogleOAuthClientID     string
+	GoogleOAuthClientSecret string
+	RedirectURI             string
+	Code                    string
+}
+
+type RedeemCodeResponse struct {
+	GoogleUserID       string
+	Email              string
+	GoogleHostedDomain string
+}
+
+func (c *Client) RedeemCode(ctx context.Context, req *RedeemCodeRequest) (*RedeemCodeResponse, error) {
+	accessToken, err := c.token(ctx, req)
+	if err != nil {
+		return nil, fmt.Errorf("redeem code: %w", err)
+	}
+
+	userInfo, err := c.userinfo(ctx, accessToken)
+	if err != nil {
+		return nil, fmt.Errorf("userinfo: %w", err)
+	}
+
+	return &RedeemCodeResponse{
+		GoogleUserID:       userInfo.Sub,
+		Email:              userInfo.Email,
+		GoogleHostedDomain: userInfo.HD,
+	}, nil
+}
+
+func (c *Client) token(ctx context.Context, req *RedeemCodeRequest) (string, error) {
+	body := url.Values{}
+	body.Set("client_id", req.GoogleOAuthClientID)
+	body.Set("client_secret", req.GoogleOAuthClientSecret)
+	body.Set("code", req.Code)
+	body.Set("redirect_uri", req.RedirectURI)
+	body.Set("grant_type", "authorization_code")
+
+	httpReq, err := http.NewRequestWithContext(ctx, http.MethodPost, "https://oauth2.googleapis.com/token", strings.NewReader(body.Encode()))
+	if err != nil {
+		return "", fmt.Errorf("new http request: %w", err)
+	}
+
+	httpReq.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+
+	httpRes, err := c.HTTPClient.Do(httpReq)
+	if err != nil {
+		return "", fmt.Errorf("send http request: %w", err)
+	}
+	defer httpRes.Body.Close()
+
+	if httpRes.StatusCode != http.StatusOK {
+		return "", fmt.Errorf("bad response status code: %s", httpRes.Status)
+	}
+
+	resBody, err := io.ReadAll(httpRes.Body)
+	if err != nil {
+		return "", fmt.Errorf("read body: %w", err)
+	}
+
+	var data struct {
+		AccessToken string `json:"access_token"`
+	}
+	if err := json.Unmarshal(resBody, &data); err != nil {
+		return "", fmt.Errorf("unmarshal response: %w", err)
+	}
+
+	return data.AccessToken, nil
+}
+
+type userinfoResponse struct {
+	Sub   string `json:"sub"`
+	Email string `json:"email"`
+	HD    string `json:"hd"`
+}
+
+func (c *Client) userinfo(ctx context.Context, accessToken string) (*userinfoResponse, error) {
+	httpReq, err := http.NewRequestWithContext(ctx, http.MethodGet, "https://openidconnect.googleapis.com/v1/userinfo", nil)
+	if err != nil {
+		return nil, fmt.Errorf("new http request: %w", err)
+	}
+
+	httpReq.Header.Add("Authorization", "Bearer "+accessToken)
+
+	httpRes, err := c.HTTPClient.Do(httpReq)
+	if err != nil {
+		return nil, fmt.Errorf("send http request: %w", err)
+	}
+	defer httpRes.Body.Close()
+
+	if httpRes.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("bad response status code: %s", httpRes.Status)
+	}
+
+	resBody, err := io.ReadAll(httpRes.Body)
+	if err != nil {
+		return nil, fmt.Errorf("read body: %w", err)
+	}
+
+	var data userinfoResponse
+	if err := json.Unmarshal(resBody, &data); err != nil {
+		return nil, fmt.Errorf("unmarshal response: %w", err)
+	}
+
+	return &data, nil
+}
