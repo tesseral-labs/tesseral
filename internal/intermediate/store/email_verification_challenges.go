b773ed94d0d9072b8d5a0c3cb32b8d6f3bc3097a
Blake Williams <blake.williams@ssoready.com>
2024-12-03 14:07:56 -0800

    Move intermediate service and store logic

---
+++ internal/intermediate/store/email_verification_challenges.go
@@ -0,0 +1,156 @@
+package store
+
+import (
+	"context"
+	"crypto/sha256"
+	"math/rand/v2"
+	"strconv"
+	"time"
+
+	"github.com/google/uuid"
+	"github.com/openauth/openauth/internal/store/idformat"
+	"github.com/openauth/openauth/internal/store/queries"
+)
+
+type EmailVerificationChallenge struct {
+	ID                    string
+	IntermediateSessionID string
+	ProjectID             string
+	Challenge             string
+	ChallengeSha256       []byte
+	CompleteTime          time.Time
+	Email                 string
+	ExpireTime            time.Time
+	GoogleUserID          string
+	MicrosoftUserID       string
+}
+
+type CreateEmailVerificationChallengeParams struct {
+	ChallengeSha256       []byte
+	Email                 string
+	GoogleUserID          string
+	IntermediateSessionID string
+	MicrosoftUserID       string
+	ProjectID             string
+}
+
+type GetEmailVerificationChallengeParams struct {
+	Code                  string
+	Email                 string
+	GoogleUserID          string
+	IntermediateSessionID string
+	MicrosoftUserID       string
+	ProjectID             string
+}
+
+func (s *Store) CreateEmailVerificationChallenge(ctx context.Context, params *CreateEmailVerificationChallengeParams) (*EmailVerificationChallenge, error) {
+	_, q, commit, rollback, err := s.tx(ctx)
+	if err != nil {
+		return nil, err
+	}
+	defer rollback()
+
+	intermediateSessionID, err := idformat.IntermediateSession.Parse(params.IntermediateSessionID)
+	if err != nil {
+		return nil, err
+	}
+
+	projectID, err := idformat.Project.Parse(params.ProjectID)
+	if err != nil {
+		return nil, err
+	}
+
+	// Create a new secret token for the challenge
+	secretToken, err := generateSecretToken()
+	if err != nil {
+		return nil, err
+	}
+	secretTokenSha256 := sha256.Sum256([]byte(secretToken))
+
+	// TODO: Send the secret token to the user's email address
+
+	expiresAt := time.Now().Add(15 * time.Minute)
+
+	evc, err := q.CreateEmailVerificationChallenge(ctx, queries.CreateEmailVerificationChallengeParams{
+		ID:                    uuid.New(),
+		IntermediateSessionID: intermediateSessionID,
+		ProjectID:             projectID,
+		ChallengeSha256:       secretTokenSha256[:],
+		Email:                 &params.Email,
+		ExpireTime:            &expiresAt,
+		GoogleUserID:          &params.GoogleUserID,
+		MicrosoftUserID:       &params.MicrosoftUserID,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	if err := commit(); err != nil {
+		return nil, err
+	}
+
+	return parseEmailVerificationChallenge(&evc, secretToken), nil
+}
+
+func (s *Store) GetEmailVerificationChallenge(ctx context.Context, params *GetEmailVerificationChallengeParams) (*EmailVerificationChallenge, error) {
+	_, q, _, rollback, err := s.tx(ctx)
+	if err != nil {
+		return nil, err
+	}
+	defer rollback()
+
+	now := time.Now()
+
+	intermediateSessionID, err := idformat.IntermediateSession.Parse(params.IntermediateSessionID)
+	if err != nil {
+		return nil, err
+	}
+
+	projectID, err := idformat.Project.Parse(params.ProjectID)
+	if err != nil {
+		return nil, err
+	}
+
+	secretTokenSha256 := sha256.Sum256([]byte(params.Code))
+
+	evc, err := q.GetEmailVerificationChallenge(ctx, queries.GetEmailVerificationChallengeParams{
+		ExpireTime:            &now,
+		ChallengeSha256:       secretTokenSha256[:],
+		Email:                 &params.Email,
+		GoogleUserID:          &params.GoogleUserID,
+		IntermediateSessionID: intermediateSessionID,
+		MicrosoftUserID:       &params.MicrosoftUserID,
+		ProjectID:             projectID,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	return parseEmailVerificationChallenge(&evc, ""), nil
+}
+
+func generateSecretToken() (string, error) {
+	// Define the range for a 6-digit number: [100000, 999999]
+	min := 100000
+	max := 999999
+
+	// Generate a secure random number
+	randomNumber := rand.IntN(max-min+1) + min
+
+	return strconv.Itoa(randomNumber), nil
+}
+
+func parseEmailVerificationChallenge(evc *queries.EmailVerificationChallenge, originalChallenge string) *EmailVerificationChallenge {
+	return &EmailVerificationChallenge{
+		ID:                    idformat.EmailVerificationChallenge.Format(evc.ID),
+		IntermediateSessionID: idformat.IntermediateSession.Format(evc.IntermediateSessionID),
+		ProjectID:             idformat.Project.Format(evc.ProjectID),
+		Challenge:             originalChallenge,
+		ChallengeSha256:       evc.ChallengeSha256,
+		CompleteTime:          *evc.CompleteTime,
+		Email:                 *evc.Email,
+		ExpireTime:            *evc.ExpireTime,
+		GoogleUserID:          *evc.GoogleUserID,
+		MicrosoftUserID:       *evc.MicrosoftUserID,
+	}
+}
---
+++ internal/intermediate/store/intermediate_session_signing_keys.go
@@ -0,0 +1,202 @@
+package store
+
+import (
+	"context"
+	"crypto/ecdsa"
+	"time"
+
+	"github.com/aws/aws-sdk-go-v2/service/kms"
+	"github.com/aws/aws-sdk-go-v2/service/kms/types"
+	"github.com/google/uuid"
+	openauthecdsa "github.com/openauth/openauth/internal/crypto/ecdsa"
+	"github.com/openauth/openauth/internal/store/idformat"
+	"github.com/openauth/openauth/internal/store/queries"
+)
+
+type IntermediateSessionSigningKey struct {
+	ID         uuid.UUID
+	ProjectID  uuid.UUID
+	CreateTime time.Time
+	ExpireTime time.Time
+	PublicKey  *ecdsa.PublicKey
+	PrivateKey *ecdsa.PrivateKey
+}
+
+func (s *Store) CreateIntermediateSessionSigningKey(ctx context.Context, projectId string) (*IntermediateSessionSigningKey, error) {
+	_, q, commit, rollback, err := s.tx(ctx)
+	if err != nil {
+		return nil, err
+	}
+	defer rollback()
+
+	projectID, err := idformat.Project.Parse(projectId)
+	if err != nil {
+		return nil, err
+	}
+
+	// Allow this key to be used for 7 hours
+	// - this adds a 1 hour buffer to the 6 hour key rotation period,
+	//   so that the key can be rotated before it expires without
+	//   causing existing JWT parsing to fail
+	expiresAt := time.Now().Add(time.Hour * 7)
+
+	// Generate a new symmetric key
+	privateKey, err := openauthecdsa.GenerateKey()
+	if err != nil {
+		return nil, err
+	}
+
+	privateKeyBytes, err := openauthecdsa.PrivateKeyBytes(privateKey)
+	if err != nil {
+		return nil, err
+	}
+
+	// Encrypt the symmetric key with the KMS
+	encryptOutput, err := s.kms.Encrypt(ctx, &kms.EncryptInput{
+		EncryptionAlgorithm: types.EncryptionAlgorithmSpecRsaesOaepSha256,
+		KeyId:               &s.intermediateSessionSigningKeyKMSKeyID,
+		Plaintext:           privateKeyBytes,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	publicKeyBytes, err := openauthecdsa.PublicKeyBytes(&privateKey.PublicKey)
+	if err != nil {
+		return nil, err
+	}
+
+	// Store the encrypted key in the database
+	createdIntermediateSessionSigningKey, err := q.CreateIntermediateSessionSigningKey(ctx, queries.CreateIntermediateSessionSigningKeyParams{
+		ID:                   uuid.New(),
+		ProjectID:            projectID,
+		ExpireTime:           &expiresAt,
+		PublicKey:            publicKeyBytes,
+		PrivateKeyCipherText: encryptOutput.CipherTextBlob,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	if err := commit(); err != nil {
+		return nil, err
+	}
+
+	return parseIntermediateSessionSigningKey(&createdIntermediateSessionSigningKey, privateKey), nil
+}
+
+func (s *Store) GetIntermediateSessionSigningKeyByID(ctx context.Context, id string) (*IntermediateSessionSigningKey, error) {
+	_, q, _, rollback, err := s.tx(ctx)
+	if err != nil {
+		return nil, err
+	}
+	defer rollback()
+
+	intermediateSessionSigningKeyID, err := idformat.IntermediateSessionSigningKey.Parse(id)
+	if err != nil {
+		return nil, err
+	}
+
+	// Fetch the raw record from the database
+	intermediateSessionSigningKey, err := q.GetIntermediateSessionSigningKeyByID(ctx, intermediateSessionSigningKeyID)
+	if err != nil {
+		return nil, err
+	}
+
+	// Decrypt the signing key using KMS
+	decryptOutput, err := s.kms.Decrypt(ctx, &kms.DecryptInput{
+		CiphertextBlob:      intermediateSessionSigningKey.PrivateKeyCipherText,
+		EncryptionAlgorithm: types.EncryptionAlgorithmSpecRsaesOaepSha256,
+		KeyId:               &s.intermediateSessionSigningKeyKMSKeyID,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	// Create an ECDSA key pair from the decrypted signing key
+	privateKey, err := openauthecdsa.PrivateKeyFromBytes(decryptOutput.Value)
+	if err != nil {
+		return nil, err
+	}
+
+	// Return the intermediate session signing key with the decrypted private key
+	return parseIntermediateSessionSigningKey(&intermediateSessionSigningKey, privateKey), nil
+}
+
+func (s *Store) GetIntermediateSessionSigningKeyByProjectID(ctx context.Context, projectId string) (*IntermediateSessionSigningKey, error) {
+	_, q, _, rollback, err := s.tx(ctx)
+	if err != nil {
+		return nil, err
+	}
+	defer rollback()
+
+	projectID, err := idformat.Project.Parse(projectId)
+	if err != nil {
+		return nil, err
+	}
+
+	// Fetch the raw record from the database
+	intermediateSessionSigningKey, err := q.GetIntermediateSessionSigningKeyByProjectID(ctx, projectID)
+	if err != nil {
+		return nil, err
+	}
+
+	// Decrypt the signing key using KMS
+	decryptOutput, err := s.kms.Decrypt(ctx, &kms.DecryptInput{
+		CiphertextBlob:      intermediateSessionSigningKey.PrivateKeyCipherText,
+		EncryptionAlgorithm: types.EncryptionAlgorithmSpecRsaesOaepSha256,
+		KeyId:               &s.intermediateSessionSigningKeyKMSKeyID,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	// Create an ECDSA key pair from the decrypted signing key
+	privateKey, err := openauthecdsa.PrivateKeyFromBytes(decryptOutput.Value)
+	if err != nil {
+		return nil, err
+	}
+
+	// Return the intermediate session signing key with the decrypted private key
+	return parseIntermediateSessionSigningKey(&intermediateSessionSigningKey, privateKey), nil
+}
+
+func (s *Store) GetIntermediateSessionPublicKeyByProjectID(ctx context.Context, projectId string) (*ecdsa.PublicKey, error) {
+	_, q, _, rollback, err := s.tx(ctx)
+	if err != nil {
+		return nil, err
+	}
+	defer rollback()
+
+	projectID, err := idformat.Project.Parse(projectId)
+	if err != nil {
+		return nil, err
+	}
+
+	// Fetch the raw record from the database
+	intermediateSessionSigningKey, err := q.GetIntermediateSessionSigningKeyByProjectID(ctx, projectID)
+	if err != nil {
+		return nil, err
+	}
+
+	// Create an ECDSA key pair from the decrypted signing key
+	publicKey, err := openauthecdsa.PublicKeyFromBytes(intermediateSessionSigningKey.PublicKey)
+	if err != nil {
+		return nil, err
+	}
+
+	return publicKey, nil
+}
+
+func parseIntermediateSessionSigningKey(issk *queries.IntermediateSessionSigningKey, privateKey *ecdsa.PrivateKey) *IntermediateSessionSigningKey {
+	publicKey := &privateKey.PublicKey
+
+	return &IntermediateSessionSigningKey{
+		ID:         issk.ID,
+		ProjectID:  issk.ProjectID,
+		CreateTime: *issk.CreateTime,
+		ExpireTime: *issk.ExpireTime,
+		PublicKey:  publicKey,
+		PrivateKey: privateKey,
+	}
+}
