ef38665acca31870c2b7dba634c27d95370c6503
Blake Williams <blakethewilliams@gmail.com>
2024-12-04 08:33:56 -0800

    Move oauth service logic (#38)
    Moves `oauthservice` and `oauthstore` to `internal/oauth`.

--- sqlc/queries.sql
+++ sqlc/queries.sql
@@ -61,14 +61,6 @@ FROM
 WHERE
     id = $1;
 
--- name: GetSessionSigningKeysByProjectID :many
-SELECT
-    *
-FROM
-    session_signing_keys
-WHERE
-    project_id = $1;
-
 -- name: GetOrganizationByGoogleHostedDomain :one
 SELECT
     *
