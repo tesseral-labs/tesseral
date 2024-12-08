b3b275b5ecd3006be990bc8d1527a8d157b8db1a
Ulysse Carion <ulysse.carion@ssoready.com>
2024-12-07 18:15:12 -0800

    Add hibp client

---
+++ internal/hibp/client_test.go
@@ -0,0 +1,71 @@
+package hibp
+
+import (
+	"context"
+	"fmt"
+	"math/rand/v2"
+	"net/http"
+	"strconv"
+	"testing"
+)
+
+func ExampleClient_Pwned() {
+	c := Client{HTTPClient: http.DefaultClient}
+	randomPassword := strconv.FormatFloat(rand.Float64(), 'f', -1, 64)
+	fmt.Println(c.Pwned(context.Background(), "letmein"))
+	fmt.Println(c.Pwned(context.Background(), randomPassword))
+	// Output:
+	// true <nil>
+	// false <nil>
+}
+
+func TestCheckLineMatch(t *testing.T) {
+	tests := []struct {
+		name          string
+		sha1HexSuffix string
+		line          string
+		want          bool
+		wantErr       bool
+	}{
+		{
+			name:          "match",
+			sha1HexSuffix: "suffix",
+			line:          "suffix:1",
+			want:          true,
+			wantErr:       false,
+		},
+		{
+			name:          "different suffix",
+			sha1HexSuffix: "suffix",
+			line:          "notsuffix:1",
+			want:          false,
+			wantErr:       false,
+		},
+		{
+			name:          "padding line",
+			sha1HexSuffix: "suffix",
+			line:          "suffix:0",
+			want:          false,
+			wantErr:       false,
+		},
+		{
+			name:          "malformed line",
+			sha1HexSuffix: "suffix",
+			line:          "suffix:1:1",
+			want:          false,
+			wantErr:       true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := checkLineMatch(tt.sha1HexSuffix, tt.line)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("checkLineMatch() error = %v, wantErr %v", err, tt.wantErr)
+			}
+			if got != tt.want {
+				t.Errorf("checkLineMatch() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
