07f613af191f17196ee747c3a212fe3002a0f1bd
Blake Williams <blake.williams@ssoready.com>
2024-12-12 14:37:42 -0800

    Simplify config replacement

--- ui/build.mjs
+++ ui/build.mjs
@@ -1,5 +1,4 @@
 import * as esbuild from 'esbuild'
-import { replace } from 'esbuild-plugin-replace'
 import { configDotenv } from 'dotenv'
 
 const UI_BUILD_IS_DEV = process.env.UI_BUILD_IS_DEV === '1'
@@ -12,17 +11,15 @@ if (UI_BUILD_IS_DEV) {
 
 const context = await esbuild.context({
   bundle: true,
+  define: {
+    __REPLACED_BY_ESBUILD_API_URL__: JSON.stringify(process.env.UI_API_URL),
+    __REPLACED_BY_ESBUILD_PROJECT_ID__: JSON.stringify(
+      process.env.UI_PROJECT_ID,
+    ),
+  },
   entryPoints: ['./src'],
   minify: !UI_BUILD_IS_DEV,
   outfile: './public/index.js',
-  plugins: [
-    replace({
-      __REPLACED_BY_ESBUILD_API_URL__: JSON.stringify(process.env.UI_API_URL),
-      __REPLACED_BY_ESBUILD_PROJECT_ID__: JSON.stringify(
-        process.env.UI_PROJECT_ID,
-      ),
-    }),
-  ],
   sourcemap: true,
   target: ['chrome58', 'firefox57', 'safari11', 'edge18'],
 })
