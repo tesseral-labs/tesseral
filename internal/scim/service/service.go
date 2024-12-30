e20ccae900f5212462a618ae6d62d43856061e45
Ulysse Carion <ulysse.carion@ssoready.com>
2024-12-30 10:32:20 -0800

    [OPEN-33] [OPEN-34] [OPEN-35] [OPEN-36] [OPEN-37] [OPEN-38] Add SCIM support for Okta and Entra provision/deprovision (#72)
    This PR adds support for the Okta and Entra flows for provisioning and
    deprovisioning users, as well as updating user properties.
    
    Most of this code is copied over from SSOReady, or adapted from there.

--- internal/scim/service/service.go
+++ internal/scim/service/service.go
@@ -1,16 +1,19 @@
 package service
 
 import (
+	"bytes"
 	"encoding/json"
 	"errors"
 	"fmt"
 	"io"
+	"log/slog"
 	"net/http"
+	"net/http/httptest"
 	"net/url"
 	"regexp"
 	"strconv"
 
-	"github.com/openauth/openauth/internal/scim/authn/middleware"
+	"github.com/openauth/openauth/internal/scim/authn/authnmiddleware"
 	"github.com/openauth/openauth/internal/scim/store"
 )
 
@@ -25,8 +28,10 @@ func (s *Service) Handler() http.Handler {
 	mux.Handle("GET /scim/v1/Users/{userID}", withErr(s.getUser))
 	mux.Handle("POST /scim/v1/Users", withErr(s.createUser))
 	mux.Handle("PUT /scim/v1/Users/{userID}", withErr(s.updateUser))
+	mux.Handle("PATCH /scim/v1/Users/{userID}", withErr(s.patchUser))
+	mux.Handle("DELETE /scim/v1/Users/{userID}", withErr(s.deleteUser))
 
-	return middleware.New(s.Store, mux)
+	return logHTTP(authnmiddleware.New(s.Store, mux))
 }
 
 var (
@@ -81,6 +86,16 @@ func (s *Service) getUser(w http.ResponseWriter, r *http.Request) error {
 
 	user, err := s.Store.GetUser(ctx, r.PathValue("userID"))
 	if err != nil {
+		var scimError *store.SCIMError
+		if errors.As(err, &scimError) {
+			w.Header().Set("Content-Type", "application/scim+json")
+			w.WriteHeader(scimError.Status)
+			if err := json.NewEncoder(w).Encode(scimError); err != nil {
+				return fmt.Errorf("write response: %w", err)
+			}
+			return nil
+		}
+
 		return fmt.Errorf("store: %w", err)
 	}
 
@@ -107,9 +122,9 @@ func (s *Service) createUser(w http.ResponseWriter, r *http.Request) error {
 		return nil
 	}
 
-	user, err := s.Store.CreateUser(ctx, &reqUser)
+	user, err := s.Store.CreateUser(ctx, reqUser)
 	if err != nil {
-		var errBadDomain *store.BadEmailError
+		var errBadDomain *store.SCIMError
 		if errors.As(err, &errBadDomain) {
 			w.Header().Set("Content-Type", "application/scim+json")
 			w.WriteHeader(http.StatusBadRequest)
@@ -145,11 +160,9 @@ func (s *Service) updateUser(w http.ResponseWriter, r *http.Request) error {
 		return nil
 	}
 
-	reqUser.ID = r.PathValue("userID")
-
-	user, err := s.Store.UpdateUser(ctx, &reqUser)
+	user, err := s.Store.UpdateUser(ctx, r.PathValue("userID"), reqUser)
 	if err != nil {
-		var errBadDomain *store.BadEmailError
+		var errBadDomain *store.SCIMError
 		if errors.As(err, &errBadDomain) {
 			w.Header().Set("Content-Type", "application/scim+json")
 			w.WriteHeader(http.StatusBadRequest)
@@ -170,11 +183,99 @@ func (s *Service) updateUser(w http.ResponseWriter, r *http.Request) error {
 	return nil
 }
 
+func (s *Service) patchUser(w http.ResponseWriter, r *http.Request) error {
+	ctx := r.Context()
+
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, fmt.Sprintf("read body: %s", err), http.StatusBadRequest)
+		return nil
+	}
+
+	var operations store.PatchOperations
+	if err := json.Unmarshal(body, &operations); err != nil {
+		http.Error(w, fmt.Sprintf("unmarshal body: %s", err), http.StatusBadRequest)
+		return nil
+	}
+
+	user, err := s.Store.PatchUser(ctx, r.PathValue("userID"), operations)
+	if err != nil {
+		var scimError *store.SCIMError
+		if errors.As(err, &scimError) {
+			w.Header().Set("Content-Type", "application/scim+json")
+			w.WriteHeader(http.StatusBadRequest)
+			if err := json.NewEncoder(w).Encode(scimError); err != nil {
+				return fmt.Errorf("write response: %w", err)
+			}
+			return nil
+		}
+
+		return fmt.Errorf("store: %w", err)
+	}
+
+	w.Header().Set("Content-Type", "application/scim+json")
+	w.WriteHeader(http.StatusOK)
+	if err := json.NewEncoder(w).Encode(user); err != nil {
+		return fmt.Errorf("write response: %w", err)
+	}
+	return nil
+}
+
+func (s *Service) deleteUser(w http.ResponseWriter, r *http.Request) error {
+	ctx := r.Context()
+	if err := s.Store.DeleteUser(ctx, r.PathValue("userID")); err != nil {
+		var scimError *store.SCIMError
+		if errors.As(err, &scimError) {
+			w.Header().Set("Content-Type", "application/scim+json")
+			w.WriteHeader(http.StatusBadRequest)
+			if err := json.NewEncoder(w).Encode(scimError); err != nil {
+				return fmt.Errorf("write response: %w", err)
+			}
+			return nil
+		}
+
+		return fmt.Errorf("store: %w", err)
+	}
+
+	w.Header().Set("Content-Type", "application/scim+json")
+	w.WriteHeader(http.StatusNoContent)
+	return nil
+}
+
 func withErr(f func(w http.ResponseWriter, r *http.Request) error) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		if err := f(w, r); err != nil {
 			http.Error(w, "", http.StatusInternalServerError)
 			panic(err)
+		}
+	})
+}
+
+func logHTTP(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		defer func() { _ = r.Body.Close() }()
+		body, err := io.ReadAll(r.Body)
+		if err != nil {
+			panic(fmt.Errorf("read body: %w", err))
+		}
+
+		// log request before ServeHTTP in case that panics
+		slog.InfoContext(r.Context(), "http_request", "method", r.Method, "path", r.URL.Path, "request_body", string(body))
+
+		// rewrite the response to be a recorded one, and the request to have the original body
+		recorder := httptest.NewRecorder()
+		r.Body = io.NopCloser(bytes.NewBuffer(body))
+		h.ServeHTTP(recorder, r)
+
+		slog.InfoContext(r.Context(), "http_response", "method", r.Method, "path", r.URL.Path, "request_body", string(body), "response_status", recorder.Code, "response_headers", recorder.Header(), "response_body", recorder.Body.String())
+
+		// write out recorded response to w
+		for k, v := range recorder.Header() {
+			w.Header()[k] = v
+		}
+		w.WriteHeader(recorder.Code)
+		if _, err := recorder.Body.WriteTo(w); err != nil {
+			panic(fmt.Errorf("write body: %w", err))
 		}
 	})
 }
--- internal/scim/store/users.go
+++ internal/scim/store/users.go
@@ -2,6 +2,7 @@ package store
 
 import (
 	"context"
+	"encoding/json"
 	"errors"
 	"fmt"
 	"net/http"
@@ -13,6 +14,7 @@ import (
 	"github.com/jackc/pgx/v5/pgconn"
 	"github.com/openauth/openauth/internal/emailaddr"
 	"github.com/openauth/openauth/internal/scim/authn"
+	"github.com/openauth/openauth/internal/scim/internal/scimpatch"
 	"github.com/openauth/openauth/internal/scim/store/queries"
 	"github.com/openauth/openauth/internal/store/idformat"
 )
@@ -26,14 +28,22 @@ type ListUsersRequest struct {
 type ListUsersResponse struct {
 	Schemas      []string `json:"schemas,omitempty"`
 	TotalResults int      `json:"totalResults"`
-	Users        []*User  `json:"Resources"`
+	Users        []User   `json:"Resources"`
 }
 
-type User struct {
+// User is a SCIM representation of a user. It is suitable for JSON
+// serialization.
+type User any
+
+// parsedUser is our preferred representation of SCIM users.
+//
+// Most IDPs will sometimes use different representations of users. Entra, in
+// particular, sends "active" as a string instead of a boolean.
+type parsedUser struct {
 	Schemas  []string `json:"schemas,omitempty"`
 	ID       string   `json:"id"`
-	Active   bool     `json:"active"`
 	UserName string   `json:"userName"`
+	Active   bool     `json:"active"`
 }
 
 func (s *Store) ListUsers(ctx context.Context, req *ListUsersRequest) (*ListUsersResponse, error) {
@@ -51,18 +61,18 @@ func (s *Store) ListUsers(ctx context.Context, req *ListUsersRequest) (*ListUser
 		if err != nil {
 			if errors.Is(err, pgx.ErrNoRows) {
 				return &ListUsersResponse{
-					Schemas:      []string{"urn:ietf:params:scim:schemas:core:2.0:User"},
+					Schemas:      []string{"urn:ietf:params:scim:api:messages:2.0:ListResponse"},
 					TotalResults: 0,
-					Users:        []*User{},
+					Users:        []User{},
 				}, nil
 			}
 			return nil, fmt.Errorf("get user by email: %w", err)
 		}
 
 		return &ListUsersResponse{
-			Schemas:      []string{"urn:ietf:params:scim:schemas:core:2.0:User"},
+			Schemas:      []string{"urn:ietf:params:scim:api:messages:2.0:ListResponse"},
 			TotalResults: 1,
-			Users:        []*User{parseUser(false, qUser)},
+			Users:        []User{formatUser(false, qUser)},
 		}, nil
 	}
 
@@ -90,20 +100,85 @@ func (s *Store) ListUsers(ctx context.Context, req *ListUsersRequest) (*ListUser
 		return nil, fmt.Errorf("list users: %w", err)
 	}
 
-	users := []*User{} // intentionally not initialized as nil to avoid a JSON `null`
+	users := []User{} // intentionally not initialized as nil to avoid a JSON `null`
 	for _, qUser := range qUsers {
-		users = append(users, parseUser(false, qUser))
+		users = append(users, formatUser(false, qUser))
 	}
 
 	return &ListUsersResponse{
-		Schemas:      []string{"urn:ietf:params:scim:schemas:core:2.0:User"},
+		Schemas:      []string{"urn:ietf:params:scim:api:messages:2.0:ListResponse"},
 		TotalResults: int(count),
 		Users:        users,
 	}, nil
 }
 
-func (s *Store) GetUser(ctx context.Context, id string) (*User, error) {
+func (s *Store) GetUser(ctx context.Context, id string) (User, error) {
 	_, q, _, rollback, err := s.tx(ctx)
+	if err != nil {
+		return nil, err
+	}
+	defer rollback()
+
+	userID, err := idformat.User.Parse(id)
+	if err != nil {
+		return nil, &SCIMError{
+			Status: http.StatusBadRequest,
+			Detail: fmt.Sprintf("invalid user id: %v", err),
+		}
+	}
+
+	qUser, err := q.GetUserByID(ctx, queries.GetUserByIDParams{
+		OrganizationID: authn.OrganizationID(ctx),
+		ID:             userID,
+	})
+	if err != nil {
+		if errors.Is(err, pgx.ErrNoRows) {
+			return nil, &SCIMError{
+				Status: http.StatusNotFound,
+				Detail: "user not found",
+			}
+		}
+
+		return nil, fmt.Errorf("get user by id: %w", err)
+	}
+
+	return formatUser(true, qUser), nil
+}
+
+func (s *Store) CreateUser(ctx context.Context, user User) (User, error) {
+	_, q, commit, rollback, err := s.tx(ctx)
+	if err != nil {
+		return nil, err
+	}
+	defer rollback()
+
+	parsed, err := parseUser(user)
+	if err != nil {
+		return nil, fmt.Errorf("parse user: %w", err)
+	}
+
+	if err := s.validateEmailDomain(ctx, q, parsed.UserName); err != nil {
+		return nil, fmt.Errorf("validate email domain: %w", err)
+	}
+
+	qUser, err := q.CreateUser(ctx, queries.CreateUserParams{
+		ID:             uuid.New(),
+		OrganizationID: authn.OrganizationID(ctx),
+		Email:          parsed.UserName,
+	})
+	if err != nil {
+		return nil, fmt.Errorf("create user: %w", err)
+	}
+
+	if err := commit(); err != nil {
+		return nil, fmt.Errorf("commit: %w", err)
+	}
+
+	return formatUser(true, qUser), nil
+}
+
+func (s *Store) UpdateUser(ctx context.Context, id string, user User) (User, error) {
+	_, q, commit, rollback, err := s.tx(ctx)
 	if err != nil {
 		return nil, err
 	}
@@ -114,63 +189,18 @@ func (s *Store) GetUser(ctx context.Context, id string) (*User, error) {
 		return nil, fmt.Errorf("parse user id: %w", err)
 	}
 
-	qUser, err := q.GetUserByID(ctx, queries.GetUserByIDParams{
-		OrganizationID: authn.OrganizationID(ctx),
-		ID:             userID,
-	})
+	parsed, err := parseUser(user)
 	if err != nil {
-		return nil, fmt.Errorf("get user by id: %w", err)
+		return nil, fmt.Errorf("parse user: %w", err)
 	}
 
-	return parseUser(true, qUser), nil
-}
-
-func (s *Store) CreateUser(ctx context.Context, req *User) (*User, error) {
-	_, q, commit, rollback, err := s.tx(ctx)
-	if err != nil {
-		return nil, err
-	}
-	defer rollback()
-
-	if err := s.validateEmailDomain(ctx, q, req.UserName); err != nil {
-		return nil, fmt.Errorf("validate email domain: %w", err)
-	}
-
-	qUser, err := q.CreateUser(ctx, queries.CreateUserParams{
-		ID:             uuid.New(),
-		OrganizationID: authn.OrganizationID(ctx),
-		Email:          req.UserName,
-	})
-	if err != nil {
-		return nil, fmt.Errorf("create user: %w", err)
-	}
-
-	if err := commit(); err != nil {
-		return nil, fmt.Errorf("commit: %w", err)
-	}
-
-	return parseUser(true, qUser), nil
-}
-
-func (s *Store) UpdateUser(ctx context.Context, req *User) (*User, error) {
-	_, q, commit, rollback, err := s.tx(ctx)
-	if err != nil {
-		return nil, err
-	}
-	defer rollback()
-
-	userID, err := idformat.User.Parse(req.ID)
-	if err != nil {
-		return nil, fmt.Errorf("parse user id: %w", err)
-	}
-
-	if err := s.validateEmailDomain(ctx, q, req.UserName); err != nil {
+	if err := s.validateEmailDomain(ctx, q, parsed.UserName); err != nil {
 		return nil, fmt.Errorf("validate email domain: %w", err)
 	}
 
 	// todo do we care about this bumping deactivate_time any time an update happens to the user?
 	var deactivateTime *time.Time
-	if !req.Active {
+	if !parsed.Active {
 		now := time.Now()
 		deactivateTime = &now
 	}
@@ -179,13 +209,13 @@ func (s *Store) UpdateUser(ctx context.Context, req *User) (*User, error) {
 		OrganizationID: authn.OrganizationID(ctx),
 		ID:             userID,
 		DeactivateTime: deactivateTime,
-		Email:          req.UserName,
+		Email:          parsed.UserName,
 	})
 	if err != nil {
 		var pgxErr *pgconn.PgError
 		if errors.As(err, &pgxErr) {
 			if pgxErr.Code == "23505" && pgxErr.ConstraintName == "users_organization_id_email_key" {
-				return nil, &BadEmailError{
+				return nil, &SCIMError{
 					Status: http.StatusBadRequest,
 					Detail: "a user with that email already exists",
 				}
@@ -199,40 +229,195 @@ func (s *Store) UpdateUser(ctx context.Context, req *User) (*User, error) {
 		return nil, fmt.Errorf("commit: %w", err)
 	}
 
-	return parseUser(true, qUser), nil
+	return formatUser(true, qUser), nil
 }
 
-func parseUser(withSchema bool, qUser queries.User) *User {
+type PatchOperations struct {
+	Operations []scimpatch.Operation `json:"Operations"`
+}
+
+func (s *Store) PatchUser(ctx context.Context, id string, operations PatchOperations) (User, error) {
+	_, q, commit, rollback, err := s.tx(ctx)
+	if err != nil {
+		return nil, err
+	}
+	defer rollback()
+
+	// load our representation of the current state
+	userID, err := idformat.User.Parse(id)
+	if err != nil {
+		return nil, &SCIMError{
+			Status: http.StatusBadRequest,
+			Detail: "invalid user id",
+		}
+	}
+
+	qUser, err := q.GetUserByID(ctx, queries.GetUserByIDParams{
+		OrganizationID: authn.OrganizationID(ctx),
+		ID:             userID,
+	})
+	if err != nil {
+		if errors.Is(err, pgx.ErrNoRows) {
+			return nil, &SCIMError{
+				Status: http.StatusNotFound,
+				Detail: "user not found",
+			}
+		}
+
+		return nil, fmt.Errorf("get user by id: %w", err)
+	}
+
+	// load current state in SCIM representation
+	scimUser := jsonify(formatUser(false, qUser))
+
+	// apply patches to that representation
+	if err := scimpatch.Patch(operations.Operations, &scimUser); err != nil {
+		return nil, fmt.Errorf("patch user: %w", err)
+	}
+
+	// convert back to preferred representation
+	parsed, err := parseUser(scimUser)
+	if err != nil {
+		fmt.Printf("fail to parse %v %v\n", operations, scimUser)
+
+		return nil, fmt.Errorf("parse patched user: %w", err)
+	}
+
+	// IDPs may deprovision a user by PATCHing away everything except "active".
+	// We always require email. So if a PATCHed scimUser lacks a userName (i.e.
+	// email), restore it.
+	if parsed.UserName == "" {
+		parsed.UserName = qUser.Email
+	}
+
+	if err := s.validateEmailDomain(ctx, q, parsed.UserName); err != nil {
+		return nil, fmt.Errorf("validate email domain: %w", err)
+	}
+
+	// todo do we care about this bumping deactivate_time any time an update happens to the user?
+	var deactivateTime *time.Time
+	if !parsed.Active {
+		now := time.Now()
+		deactivateTime = &now
+	}
+
+	qUser, err = q.UpdateUser(ctx, queries.UpdateUserParams{
+		OrganizationID: authn.OrganizationID(ctx),
+		ID:             userID,
+		DeactivateTime: deactivateTime,
+		Email:          parsed.UserName,
+	})
+	if err != nil {
+		var pgxErr *pgconn.PgError
+		if errors.As(err, &pgxErr) {
+			if pgxErr.Code == "23505" && pgxErr.ConstraintName == "users_organization_id_email_key" {
+				return nil, &SCIMError{
+					Status: http.StatusBadRequest,
+					Detail: "a user with that email already exists",
+				}
+			}
+		}
+
+		return nil, fmt.Errorf("update user: %w", err)
+	}
+
+	if err := commit(); err != nil {
+		return nil, fmt.Errorf("commit: %w", err)
+	}
+
+	return formatUser(true, qUser), nil
+}
+
+func (s *Store) DeleteUser(ctx context.Context, id string) error {
+	_, q, commit, rollback, err := s.tx(ctx)
+	if err != nil {
+		return err
+	}
+	defer rollback()
+
+	userID, err := idformat.User.Parse(id)
+	if err != nil {
+		return &SCIMError{
+			Status: http.StatusBadRequest,
+			Detail: "invalid user id",
+		}
+	}
+
+	now := time.Now()
+	if _, err := q.DeactivateUser(ctx, queries.DeactivateUserParams{
+		OrganizationID: authn.OrganizationID(ctx),
+		ID:             userID,
+		DeactivateTime: &now,
+	}); err != nil {
+		return fmt.Errorf("deactivate user: %w", err)
+	}
+
+	if err := commit(); err != nil {
+		return fmt.Errorf("commit: %w", err)
+	}
+
+	return nil
+}
+
+func parseUser(user User) (*parsedUser, error) {
+	m, ok := user.(map[string]any)
+	if !ok {
+		return nil, fmt.Errorf("users must be objects")
+	}
+
+	userName, _ := m["userName"].(string)
+
+	var active bool
+	if _, ok := m["active"]; !ok {
+		active = true
+	} else if a, ok := m["active"].(bool); ok {
+		active = a
+	} else if a, ok := m["active"].(string); ok {
+		if a == "True" {
+			active = true
+		} else if a == "False" {
+			active = false
+		} else {
+			return nil, fmt.Errorf("active must be a boolean")
+		}
+	} else {
+		return nil, fmt.Errorf("active must be a boolean")
+	}
+
+	return &parsedUser{
+		UserName: userName,
+		Active:   active,
+	}, nil
+}
+
+func formatUser(withSchema bool, qUser queries.User) User {
 	var schemas []string
 	if withSchema {
 		schemas = []string{"urn:ietf:params:scim:schemas:core:2.0:User"}
 	}
 
-	return &User{
+	return parsedUser{
 		Schemas:  schemas,
 		ID:       idformat.User.Format(qUser.ID),
-		Active:   qUser.DeactivateTime == nil,
 		UserName: qUser.Email,
+		Active:   qUser.DeactivateTime == nil,
 	}
 }
 
-// BadEmailError indicates that there was a problem with a SCIM user's email
-// address.
-//
-// Instances of BadEmailError are JSON-serializable SCIM errors.
-type BadEmailError struct {
+// SCIMError is a JSON-serializable SCIM error.
+type SCIMError struct {
 	Status int    `json:"status"`
 	Detail string `json:"detail"`
 }
 
-func (e *BadEmailError) Error() string {
-	return e.Detail
+func (e *SCIMError) Error() string {
+	return fmt.Sprintf("scim error: %d: %s", e.Status, e.Detail)
 }
 
 func (s *Store) validateEmailDomain(ctx context.Context, q *queries.Queries, email string) error {
 	domain, err := emailaddr.Parse(email)
 	if err != nil {
-		return &BadEmailError{
+		return &SCIMError{
 			Status: http.StatusBadRequest,
 			Detail: "userName must be an email address",
 		}
@@ -252,11 +437,24 @@ func (s *Store) validateEmailDomain(ctx context.Context, q *queries.Queries, ema
 	}
 
 	if !domainOk {
-		return &BadEmailError{
+		return &SCIMError{
 			Status: http.StatusBadRequest,
 			Detail: fmt.Sprintf("userName is not from the list of allowed domains: %s", strings.Join(qOrganizationDomains, ", ")),
 		}
 	}
 
 	return nil
+}
+
+func jsonify(t any) map[string]any {
+	b, err := json.Marshal(t)
+	if err != nil {
+		panic(err)
+	}
+
+	var v map[string]any
+	if err := json.Unmarshal(b, &v); err != nil {
+		panic(err)
+	}
+	return v
 }
