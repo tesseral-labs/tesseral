8ebf50d93a22c8e30a613ff926346b9903d96d78
Ulysse Carion <ulysse.carion@ssoready.com>
2025-01-09 14:01:37 -0800

    ExchangeIntermediateSessionForNewOrganizationSession: honor project saml/scim defaults

--- sqlc/queries-intermediate.sql
+++ sqlc/queries-intermediate.sql
@@ -29,8 +29,8 @@ RETURNING
     *;
 
 -- name: CreateOrganization :one
-INSERT INTO organizations (id, project_id, display_name, google_hosted_domain, microsoft_tenant_id, override_log_in_methods, override_log_in_with_google_enabled, override_log_in_with_microsoft_enabled, override_log_in_with_password_enabled)
-    VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)
+INSERT INTO organizations (id, project_id, display_name, google_hosted_domain, microsoft_tenant_id, override_log_in_methods, override_log_in_with_google_enabled, override_log_in_with_microsoft_enabled, override_log_in_with_password_enabled, saml_enabled, scim_enabled)
+    VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11)
 RETURNING
     *;
 
