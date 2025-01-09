1e45a3cbcfc24c4a13c0a99b31f70f169d0aef68
Blake Williams <blakethewilliams@gmail.com>
2025-01-09 14:15:29 -0800

    Get app building properly (#99)
    In order to get the app to build properly, this PR:
    - Updates proto gen config and app buf dependencies to resolve a silent
    bug
    - Updates nginx config file and docker-compose config to ensure service
    dependency is safe
    - Updates the build setup to use our updated config building method
    (stolen from `ui`)
    - Adds a basic router to test that session cookies are working as
    expected

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,11 +1,49 @@
 import React, { FC } from 'react'
 
-const App: FC = () => {
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { API_URL, DOGFOOD_PROJECT_ID } from './config'
+import { createConnectTransport } from '@connectrpc/connect-web'
+import { type Transport } from '@connectrpc/connect'
+import { TransportProvider } from '@connectrpc/connect-query'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import SessionInfoPage from './pages/SessionInfoPage'
+import NotFoundPage from './pages/NotFound'
+
+const queryClient = new QueryClient()
+
+function useTransport(): Transport {
+  return createConnectTransport({
+    baseUrl: `${API_URL}/internal/connect`,
+    fetch: (input, init) => fetch(input, { ...init, credentials: 'include' }),
+    interceptors: [
+      (next) => async (req) => {
+        // TODO: When we figure out how to get the project ID from the server, we should remove this logic.
+        req.header.set('X-TODO-OpenAuth-Project-ID', DOGFOOD_PROJECT_ID)
+
+        return next(req)
+      },
+    ],
+  })
+}
+
+const AppWithRoutes: FC = () => {
+  const transport = useTransport()
   return (
-    <div>
-      <h1>Hello, world!</h1>
-    </div>
+    <TransportProvider transport={transport}>
+      <QueryClientProvider client={queryClient}>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<SessionInfoPage />} />
+            <Route path="*" element={<NotFoundPage />} />
+          </Routes>
+        </BrowserRouter>
+      </QueryClientProvider>
+    </TransportProvider>
   )
+}
+
+const App: FC = () => {
+  return <AppWithRoutes />
 }
 
 export default App
