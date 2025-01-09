ae86b8f33e031c5d68f4adc5800d03a5934b86a7
Blake Williams <blakethewilliams@gmail.com>
2025-01-09 12:11:06 -0700

    Get intermediate cookies working

--- internal/intermediate/authn/interceptor/interceptor.go
+++ internal/intermediate/authn/interceptor/interceptor.go
@@ -3,15 +3,11 @@ package intermediateinterceptor
 import (
 	"context"
 	"errors"
-	"fmt"
-	"log/slog"
 
 	"connectrpc.com/connect"
 	"github.com/openauth/openauth/internal/cookies"
 	"github.com/openauth/openauth/internal/intermediate/authn"
 	"github.com/openauth/openauth/internal/intermediate/store"
-	"github.com/openauth/openauth/internal/projectid"
-	"github.com/openauth/openauth/internal/store/idformat"
 )
 
 var ErrAuthorizationHeaderRequired = errors.New("authorization header is required")
@@ -31,16 +27,11 @@ func New(s *store.Store) connect.UnaryInterceptorFunc {
 				}
 			}
 
-			projectID := projectid.ProjectID(ctx)
-			cookieName := fmt.Sprintf("tesseral:%s:intermediateAccessToken", idformat.Project.Format(projectID))
-
 			// Enforce authentication if not skipping
-			secretValue, err := cookies.GetCookie(ctx, req, cookieName)
+			secretValue, err := cookies.GetCookie(ctx, req, "intermediateAccessToken")
 			if err != nil {
 				return nil, connect.NewError(connect.CodeUnauthenticated, err)
 			}
-
-			slog.InfoContext(ctx, "authn.interceptor", "intermediateAccessToken", secretValue)
 
 			intermediateSession, err := s.GetIntermediateSessionByToken(ctx, secretValue)
 			if err != nil {
