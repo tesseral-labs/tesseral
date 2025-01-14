138b4124543ca7c01ebf78c6e6508b35c7754df4
Ulysse Carion <ulysse.carion@ssoready.com>
2025-01-14 14:06:55 -0800

    Add basic ListOrganizationsPage, ViewOrganizationPage (#122)
    This PR adds shadcn to the app, and a basic set of tables for listing
    orgs, and listing users in an org. A follow-on PR will develop out all
    such tables/pages, at which point we can begin to decide on design
    patterns.

--- app/src/pages/organizations/ListOrganizationsPage.tsx
+++ app/src/pages/organizations/ListOrganizationsPage.tsx
@@ -1,17 +1,55 @@
 import React from 'react'
 import { useQuery } from '@connectrpc/connect-query'
+import { listOrganizations } from '@/gen/openauth/backend/v1/backend-BackendService_connectquery'
 import {
-  listOrganizations
-} from '@/gen/openauth/backend/v1/backend-BackendService_connectquery'
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/components/ui/table'
+import { DateTime } from 'luxon'
+import { timestampDate } from '@bufbuild/protobuf/wkt'
+import { Link } from 'react-router-dom'
 
 export function ListOrganizationsPage() {
-  const { data: organizations } = useQuery(listOrganizations, {})
+  const { data: listOrganizationsResponse } = useQuery(listOrganizations, {})
 
   return (
     <div>
-      {organizations?.organizations?.map(org => (
-        <div key={org.id}>{org.displayName}</div>
-      ))}
+      <h1 className="font-semibold text-2xl">Organizations</h1>
+
+      <Table>
+        <TableHeader>
+          <TableRow>
+            <TableHead>Display Name</TableHead>
+            <TableHead>ID</TableHead>
+            <TableHead>Created At</TableHead>
+            <TableHead>Updated At</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {listOrganizationsResponse?.organizations?.map((org) => (
+            <TableRow key={org.id}>
+              <TableCell className="font-medium">
+                <Link to={`/organizations/${org.id}`}>{org.displayName}</Link>
+              </TableCell>
+              <TableCell className="font-mono">{org.id}</TableCell>
+              <TableCell>
+                {DateTime.fromJSDate(
+                  timestampDate(org.createTime!),
+                ).toRelative()}
+              </TableCell>
+              <TableCell>
+                {DateTime.fromJSDate(
+                  timestampDate(org.updateTime!),
+                ).toRelative()}
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
     </div>
   )
 }
