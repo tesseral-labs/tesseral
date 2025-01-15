03a2f00d2a3d127c8376200a5a43adee554d40a1
Blake Williams <blakethewilliams@gmail.com>
2025-01-14 20:30:26 -0700

    shared stored

--- internal/shared/store/projectid.go
+++ internal/shared/store/projectid.go
@@ -2,9 +2,12 @@ package store
 
 import (
 	"context"
+	"errors"
 	"fmt"
 
+	"connectrpc.com/connect"
 	"github.com/google/uuid"
+	"github.com/jackc/pgx/v5"
 )
 
 func (s *Store) GetProjectIDByDomain(ctx context.Context, domain string) (*uuid.UUID, error) {
@@ -16,6 +19,10 @@ func (s *Store) GetProjectIDByDomain(ctx context.Context, domain string) (*uuid.
 
 	qProjectID, err := q.GetProjectIDByCustomAuthDomain(ctx, &domain)
 	if err != nil {
+		if errors.Is(err, pgx.ErrNoRows) {
+			return nil, connect.NewError(connect.CodeInvalidArgument, errors.New("project not found"))
+		}
+
 		return nil, fmt.Errorf("get project id by custom auth domain: %w", err)
 	}
 
