91c9c990439f7907a9ef51153c1d81a948a01ec4
Blake Williams <blakethewilliams@gmail.com>
2025-01-14 17:19:39 -0700

    [OPEN-78] Add saml buttons after email input (#124)
    This PR adds support for fetching SAML-enabled organizations after a
    valid email is entered into the login with email input on the UI's login
    page.
    ![Screenshot 2025-01-14 at 5 11
    46 PM](https://github.com/user-attachments/assets/797bc904-69a7-4a33-b12a-7c900e7e7591)

--- ui/src/components/EmailForm.tsx
+++ ui/src/components/EmailForm.tsx
@@ -1,21 +1,53 @@
-import React, { ChangeEvent, FormEvent, useEffect, useState } from 'react'
+import React, {
+  ChangeEvent,
+  FormEvent,
+  useCallback,
+  useEffect,
+  useState,
+} from 'react'
 import { useNavigate } from 'react-router'
 import { useMutation } from '@connectrpc/connect-query'
+import debounce from 'lodash.debounce'
 
 import { setIntermediateSessionToken } from '@/auth'
 import { Button } from './ui/button'
-import { signInWithEmail } from '@/gen/openauth/intermediate/v1/intermediate-IntermediateService_connectquery'
+import {
+  listSAMLOrganizations,
+  signInWithEmail,
+} from '@/gen/openauth/intermediate/v1/intermediate-IntermediateService_connectquery'
 import { LoginViews } from '@/lib/views'
+import { Organization } from '@/gen/openauth/intermediate/v1/intermediate_pb'
+import TextDivider from './ui/TextDivider'
 
 const EmailForm = () => {
   const navigate = useNavigate()
+  const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/i
   const signInWithEmailMutation = useMutation(signInWithEmail)
+  const listSAMLOrganizationsMutation = useMutation(listSAMLOrganizations)
 
   const [email, setEmail] = useState<string>('')
   const [emailIsValid, setEmailIsValid] = useState<boolean>(false)
+  const [samlOrganizations, setSamlOrganizations] = useState<Organization[]>([])
+
+  const fetchSamlOrganizations = useCallback(
+    debounce(async () => {
+      const { organizations } = await listSAMLOrganizationsMutation.mutateAsync(
+        {
+          email,
+        },
+      )
+
+      setSamlOrganizations(organizations)
+    }, 300),
+    [email],
+  )
 
   const handleEmail = (e: ChangeEvent<HTMLInputElement>) => {
     setEmail(e.target.value)
+  }
+
+  const handleSAMLLogin = async (samlConnectId: string) => {
+    location.href = `/api/saml/v1/${samlConnectId}/init`
   }
 
   const handleSubmit = async (e: FormEvent) => {
@@ -43,30 +75,59 @@ const EmailForm = () => {
   }
 
   useEffect(() => {
-    const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/
-    setEmailIsValid(emailRegex.test(email))
+    const valid = emailRegex.test(email)
+    setEmailIsValid(valid)
+
+    if (valid) {
+      fetchSamlOrganizations()
+    }
   }, [email])
 
   return (
-    <form className="flex flex-col justify-center" onSubmit={handleSubmit}>
-      <label
-        className="text-center uppercase text-foreground font-semibold text-sm mb-6 tracking-wide"
-        htmlFor="email"
-      >
-        Continue with Email
-      </label>
-      <input
-        className="text-sm rounded border border-border focus:border-primary w-[clamp(240px,50%,100%)] mb-2"
-        id="email"
-        type="email"
-        onChange={handleEmail}
-        placeholder="jane.doe@email.com"
-        value={email}
-      />
-      <Button type="submit" disabled={!emailIsValid}>
-        Sign In
-      </Button>
-    </form>
+    <>
+      <form className="flex flex-col justify-center" onSubmit={handleSubmit}>
+        <label
+          className="text-center uppercase text-foreground font-semibold text-sm mb-6 tracking-wide"
+          htmlFor="email"
+        >
+          Continue with Email
+        </label>
+        <input
+          className="text-sm rounded border border-border focus:border-primary w-[clamp(240px,50%,100%)] mb-2"
+          id="email"
+          type="email"
+          onChange={handleEmail}
+          placeholder="jane.doe@email.com"
+          value={email}
+        />
+        <Button type="submit" disabled={!emailIsValid}>
+          Sign In
+        </Button>
+      </form>
+
+      {samlOrganizations && samlOrganizations.length > 0 && (
+        <>
+          <TextDivider text="or" />
+
+          {samlOrganizations.map((organization) => (
+            <div key={organization.id} className="flex flex-col items-center">
+              <label
+                className="text-center uppercase text-foreground font-semibold text-sm mb-3 tracking-wide w-full"
+                htmlFor="email"
+              >
+                Continue with SAML
+              </label>
+              <a
+                href={`/api/saml/v1/${organization.primarySamlConnectionId}/init`}
+                className="w-full"
+              >
+                <Button variant="outline">{organization.displayName}</Button>
+              </a>
+            </div>
+          ))}
+        </>
+      )}
+    </>
   )
 }
 
