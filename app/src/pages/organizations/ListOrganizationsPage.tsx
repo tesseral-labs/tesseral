9aa62cc9552af795dd2d7facfaddba0002dc1d43
Ulysse Carion <ulysse.carion@ssoready.com>
2025-01-15 09:48:55 -0800

    Card-ify ListOrganizations

--- app/src/pages/organizations/ListOrganizationsPage.tsx
+++ app/src/pages/organizations/ListOrganizationsPage.tsx
@@ -12,44 +12,63 @@ import {
 import { DateTime } from 'luxon'
 import { timestampDate } from '@bufbuild/protobuf/wkt'
 import { Link } from 'react-router-dom'
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card'
 
 export function ListOrganizationsPage() {
   const { data: listOrganizationsResponse } = useQuery(listOrganizations, {})
 
   return (
-    <div>
-      <h1 className="font-semibold text-2xl">Organizations</h1>
-
-      <Table>
-        <TableHeader>
-          <TableRow>
-            <TableHead>Display Name</TableHead>
-            <TableHead>ID</TableHead>
-            <TableHead>Created At</TableHead>
-            <TableHead>Updated At</TableHead>
-          </TableRow>
-        </TableHeader>
-        <TableBody>
-          {listOrganizationsResponse?.organizations?.map((org) => (
-            <TableRow key={org.id}>
-              <TableCell className="font-medium">
-                <Link to={`/organizations/${org.id}`}>{org.displayName}</Link>
-              </TableCell>
-              <TableCell className="font-mono">{org.id}</TableCell>
-              <TableCell>
-                {DateTime.fromJSDate(
-                  timestampDate(org.createTime!),
-                ).toRelative()}
-              </TableCell>
-              <TableCell>
-                {DateTime.fromJSDate(
-                  timestampDate(org.updateTime!),
-                ).toRelative()}
-              </TableCell>
+    <Card>
+      <CardHeader>
+        <CardTitle>Organizations</CardTitle>
+        <CardDescription>
+          An organization represents one of your business customers. Lorem ipsum
+          dolor.
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead>Display Name</TableHead>
+              <TableHead>ID</TableHead>
+              <TableHead>Created At</TableHead>
+              <TableHead>Updated At</TableHead>
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </div>
+          </TableHeader>
+          <TableBody>
+            {listOrganizationsResponse?.organizations?.map((org) => (
+              <TableRow key={org.id}>
+                <TableCell className="font-medium">
+                  <Link
+                    className="underline underline-offset-2 decoration-muted-foreground/40"
+                    to={`/organizations/${org.id}`}
+                  >
+                    {org.displayName}
+                  </Link>
+                </TableCell>
+                <TableCell className="font-mono">{org.id}</TableCell>
+                <TableCell>
+                  {DateTime.fromJSDate(
+                    timestampDate(org.createTime!),
+                  ).toRelative()}
+                </TableCell>
+                <TableCell>
+                  {DateTime.fromJSDate(
+                    timestampDate(org.updateTime!),
+                  ).toRelative()}
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </CardContent>
+    </Card>
   )
 }
