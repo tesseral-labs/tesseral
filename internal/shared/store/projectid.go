64eba5cd4984166284d403c2a723288a460933f2
Blake Williams <blake.williams@ssoready.com>
2025-01-15 12:15:39 -0700

    Update shared store

--- internal/shared/store/projectid.go
+++ internal/shared/store/projectid.go
@@ -5,9 +5,9 @@ import (
 	"errors"
 	"fmt"
 
-	"connectrpc.com/connect"
 	"github.com/google/uuid"
 	"github.com/jackc/pgx/v5"
+	"github.com/openauth/openauth/internal/shared/apierror"
 )
 
 func (s *Store) GetProjectIDByDomain(ctx context.Context, domain string) (*uuid.UUID, error) {
@@ -20,7 +20,7 @@ func (s *Store) GetProjectIDByDomain(ctx context.Context, domain string) (*uuid.
 	qProjectID, err := q.GetProjectIDByCustomAuthDomain(ctx, &domain)
 	if err != nil {
 		if errors.Is(err, pgx.ErrNoRows) {
-			return nil, connect.NewError(connect.CodeInvalidArgument, errors.New("project not found"))
+			return nil, apierror.NewNotFoundError("project id not found", fmt.Errorf("project id not found"))
 		}
 
 		return nil, fmt.Errorf("get project id by custom auth domain: %w", err)
