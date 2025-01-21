6cc655a7f8041023116186ed63d3a5dd99717c2d
Blake Williams <blake.williams@ssoready.com>
2025-01-21 11:32:41 -0800

    Render stuff to screen

--- ui/src/pages/dashboard/OrganizationSettingsPage.tsx
+++ ui/src/pages/dashboard/OrganizationSettingsPage.tsx
@@ -1,12 +1,41 @@
-import React, { FC } from 'react'
+import React, { FC, MouseEvent } from 'react'
 import { DateTime } from 'luxon'
-import { useOrganization, useProject } from '@/lib/auth'
+import { useOrganization, useProject, useUser } from '@/lib/auth'
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
-import { timestampDate } from '@bufbuild/protobuf/wkt'
+import { useMutation, useQuery } from '@connectrpc/connect-query'
+import {
+  listSAMLConnections,
+  listUsers,
+  updateUser,
+} from '@/gen/openauth/frontend/v1/frontend-FrontendService_connectquery'
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHeader,
+  TableRow,
+} from '@/components/ui/table'
 
 const OrganizationSettingsPage: FC = () => {
   const organization = useOrganization()
   const project = useProject()
+  const user = useUser()
+
+  const { data: usersData, refetch: refetchUsers } = useQuery(listUsers)
+  const { data: samlConnectionsData, refetch: refetchSAMLConnections } =
+    useQuery(listSAMLConnections)
+  const updateUserMutation = useMutation(updateUser)
+
+  const changeRole = async (userId: string, isOwner: boolean) => {
+    await updateUserMutation.mutateAsync({
+      id: userId,
+      user: {
+        owner: isOwner,
+      },
+    })
+
+    refetchUsers()
+  }
 
   return (
     <div className="dark:text-foreground">
@@ -16,6 +45,7 @@ const OrganizationSettingsPage: FC = () => {
           {organization?.id}
         </span>
       </div>
+
       <Card className="my-8">
         <CardHeader className="py-4">
           <CardTitle className="text-xl">General configuration</CardTitle>
@@ -47,6 +77,82 @@ const OrganizationSettingsPage: FC = () => {
               </div>
             </div>
           </div>
+        </CardContent>
+      </Card>
+
+      <Card className="my-8">
+        <CardHeader className="py-4">
+          <CardTitle className="text-xl">Users</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <Table>
+            <TableHeader>
+              <TableRow>
+                <TableCell>ID</TableCell>
+                <TableCell>Email</TableCell>
+                <TableCell>Role</TableCell>
+              </TableRow>
+            </TableHeader>
+            <TableBody>
+              {usersData?.users.map((u) => (
+                <TableRow key={u.id}>
+                  <TableCell className="flex items-center">{u.id}</TableCell>
+                  <TableCell className="text-gray-500">{u.email}</TableCell>
+                  <TableCell className="text-gray-500">
+                    {u.owner ? 'Owner' : 'Member'}
+
+                    {u.owner && u.id !== user?.id && (
+                      <div
+                        className="ml-2 rounded cursor-pointer text-primary border-border px-4 py-2 inline-block"
+                        onClick={async (e: MouseEvent<HTMLSpanElement>) => {
+                          e.stopPropagation()
+                          e.preventDefault()
+
+                          await changeRole(u.id, !u.owner)
+                        }}
+                      >
+                        Make {u.owner ? 'Member' : 'Owner'}
+                      </div>
+                    )}
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </CardContent>
+      </Card>
+
+      <Card className="my-8">
+        <CardHeader className="py-4">
+          <CardTitle className="text-xl">SAML Connections</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <Table>
+            <TableHeader>
+              <TableRow>
+                <TableCell>ID</TableCell>
+                <TableCell>IDP Entity ID</TableCell>
+                <TableCell>IDP Redirect URL</TableCell>
+                <TableCell>IDP X509 Certificate</TableCell>
+              </TableRow>
+            </TableHeader>
+            <TableBody>
+              {samlConnectionsData?.samlConnections.map((c) => (
+                <TableRow key={c.id}>
+                  <TableCell className="flex items-center">{c.id}</TableCell>
+                  <TableCell className="text-gray-500">
+                    {c.idpEntityId}
+                  </TableCell>
+                  <TableCell className="text-gray-500">
+                    {c.idpRedirectUrl}
+                  </TableCell>
+                  <TableCell className="text-gray-500">
+                    {c.idpX509Certificate ? '✓' : 'x'}
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
         </CardContent>
       </Card>
     </div>
