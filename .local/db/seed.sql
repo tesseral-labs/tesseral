8e4e52e937f55453e9263bd47d117981f65f0db7
Blake Williams <blake.williams@ssoready.com>
2025-01-24 13:32:46 -0800

    Add Project UI settings to seed.sql

--- .local/db/seed.sql
+++ .local/db/seed.sql
@@ -16,6 +16,10 @@ UPDATE projects
 INSERT INTO users (id, email, password_bcrypt, organization_id, is_owner)
   VALUES (gen_random_uuid(), 'root@app.tesseral.example.com', crypt('this_is_a_very_sensitive_password_' || encode(gen_random_bytes(16), 'hex'), gen_salt('bf', 14)), (SELECT id FROM organizations LIMIT 1), true);
 
+-- Create Project UI Settings
+INSERT INTO project_ui_settings (project_id)
+  VALUES ('56bfa2b3-4f5a-4c68-8fc5-db3bf20731a2'::uuid);
+
 -- Create a Dogfood Project API Key
 INSERT INTO project_api_keys (id, project_id, secret_token_sha256, display_name)
   VALUES (gen_random_uuid(), (SELECT id FROM projects LIMIT 1), digest(uuid_send('F938657E-65FC-4C43-B2F1-CE875A0B64D6'::uuid), 'sha256'), 'localhost');
