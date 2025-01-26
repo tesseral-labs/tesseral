1b383db52dd012d9a159292dfcae8b31da35ab6b
Ulysse Carion <ulysse.carion@ssoready.com>
2025-01-26 11:06:31 -0800

    appease linter

--- internal/backend/store/store.go
+++ internal/backend/store/store.go
@@ -13,7 +13,6 @@ import (
 	"github.com/openauth/openauth/internal/backend/store/queries"
 	"github.com/openauth/openauth/internal/common/apierror"
 	"github.com/openauth/openauth/internal/pagetoken"
-	"github.com/openauth/openauth/internal/store/idformat"
 )
 
 type Store struct {
@@ -92,41 +91,6 @@ func validateIsDogfoodSession(ctx context.Context) error {
 	data := authn.GetContextData(ctx)
 	if data.DogfoodSession == nil {
 		return apierror.NewUnauthenticatedError("this endpoint cannot be invoked by project API keys", fmt.Errorf("non-dogfood session request"))
-	}
-	return nil
-}
-
-// validateIsDogfoodSessionOfProjectOwner returns an error if the caller isn't a
-// dogfood session (i.e. a session in the app) or of an owner (i.e. an owner of
-// their respective project).
-//
-// The intention of this method is to allow endpoints to prevent themselves from
-// being called by project API keys or by non-owners.
-//
-// In production, this method validates that you're logged into app.tesseral.com
-// and that you're an owner of your Tesseral project. No other types of callers
-// are allowed.
-func (s *Store) validateIsDogfoodSessionOfProjectOwner(ctx context.Context) error {
-	if err := validateIsDogfoodSession(ctx); err != nil {
-		return fmt.Errorf("validate is dogfood session: %w", err)
-	}
-
-	data := authn.GetContextData(ctx)
-	userID, err := idformat.User.Parse(data.DogfoodSession.UserID)
-	if err != nil {
-		panic(fmt.Errorf("parse user id: %w", err))
-	}
-
-	qUser, err := s.q.GetUser(ctx, queries.GetUserParams{
-		ProjectID: authn.ProjectID(ctx),
-		ID:        userID,
-	})
-	if err != nil {
-		return fmt.Errorf("get user by id: %w", err)
-	}
-
-	if !qUser.IsOwner {
-		return apierror.NewPermissionDeniedError("user must be an owner of the organization", fmt.Errorf("user is not an owner"))
 	}
 	return nil
 }
