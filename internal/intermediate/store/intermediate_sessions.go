cd020bff2822be32c0c5eb7ed6848b35e1eeeb42
Blake Williams <blake.williams@ssoready.com>
2025-01-27 10:10:25 -0800

    Fix intermediate service

--- internal/intermediate/store/intermediate_sessions.go
+++ internal/intermediate/store/intermediate_sessions.go
@@ -64,14 +64,14 @@ func (s *Store) getIntermediateSessionEmailVerified(ctx context.Context, q *quer
 }
 
 func enforceOrganizationLoginEnabled(qOrganization queries.Organization) error {
-	if qOrganization.LoginDisabled != nil && *qOrganization.LoginDisabled {
+	if qOrganization.LoginsDisabled != nil && *qOrganization.LoginsDisabled {
 		return apierror.NewPermissionDeniedError("login disabled", fmt.Errorf("organization login disabled"))
 	}
 	return nil
 }
 
 func enforceProjectLoginEnabled(qProject queries.Project) error {
-	if qProject.LoginDisabled != nil && *qProject.LoginDisabled {
+	if qProject.LoginsDisabled != nil && *qProject.LoginsDisabled {
 		return apierror.NewPermissionDeniedError("login disabled", fmt.Errorf("project login disabled"))
 	}
 	return nil
