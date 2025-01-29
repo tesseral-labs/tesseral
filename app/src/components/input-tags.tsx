35811dbf46aac9bcc8a3ebd77c7ee985781cab34
Ulysse Carion <ulysse.carion@ssoready.com>
2025-01-29 13:37:58 -0800

    tmp before tailwind upgrade

---
+++ app/src/components/input-tags.tsx
@@ -0,0 +1,81 @@
+import { Badge } from '@/components/ui/badge'
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import { XIcon } from 'lucide-react'
+import React, { Dispatch, SetStateAction, forwardRef, useState } from 'react'
+
+type InputTagsProps = React.ComponentProps<'input'> & {
+  value: string[]
+  onChange: Dispatch<SetStateAction<string[]>>
+}
+
+export const InputTags = forwardRef<HTMLInputElement, InputTagsProps>(
+  ({ value, onChange, onBlur, ...props }, ref) => {
+    const [pendingDataPoint, setPendingDataPoint] = useState('')
+
+    const addPendingDataPoint = () => {
+      if (pendingDataPoint) {
+        // trim() because a copy-pasted input may still contain leading/trailing whitespace
+        const newDataPoints = new Set([...value, pendingDataPoint.trim()])
+        onChange(Array.from(newDataPoints))
+        setPendingDataPoint('')
+      }
+    }
+
+    return (
+      <>
+        <div className="flex gap-x-2">
+          <Input
+            value={pendingDataPoint}
+            onChange={(e) => setPendingDataPoint(e.target.value)}
+            onKeyDown={(e) => {
+              if (e.key === 'Enter') {
+                e.preventDefault()
+                addPendingDataPoint()
+              } else if (e.key === ',' || e.key === ' ') {
+                e.preventDefault()
+                addPendingDataPoint()
+              }
+            }}
+            onBlur={(e) => {
+              if (pendingDataPoint !== '') {
+                addPendingDataPoint()
+              }
+              if (onBlur) {
+                onBlur(e)
+              }
+            }}
+            {...props}
+            ref={ref}
+          />
+          <Button
+            type="button"
+            variant="secondary"
+            onClick={addPendingDataPoint}
+          >
+            Add
+          </Button>
+        </div>
+        {value.length > 0 && (
+          <div className="rounded-md min-h-[2.5rem] overflow-y-auto p-2 flex gap-2 flex-wrap items-center">
+            {value.map((item, idx) => (
+              <Badge key={idx} variant="secondary">
+                {item}
+                <button
+                  type="button"
+                  className="ml-2"
+                  onClick={() => {
+                    onChange(value.filter((i) => i !== item))
+                  }}
+                >
+                  <XIcon className="ml-2 text-muted-foreground size-2xl" />
+                </button>
+              </Badge>
+            ))}
+          </div>
+        )}
+      </>
+    )
+  },
+)
+InputTags.displayName = 'InputTags'
--- app/src/pages/organizations/OrganizationDetailsTab.tsx
+++ app/src/pages/organizations/OrganizationDetailsTab.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import {
   Card,
   CardContent,
@@ -7,13 +7,49 @@ import {
   CardTitle,
 } from '@/components/ui/card'
 import { useParams } from 'react-router'
-import { useQuery } from '@connectrpc/connect-query'
+import { useMutation, useQuery } from '@connectrpc/connect-query'
 import {
   getOrganization,
+  getOrganizationGoogleHostedDomains,
+  getOrganizationMicrosoftTenantIDs,
   getProject,
+  getProjectAPIKey,
+  updateOrganizationGoogleHostedDomains,
+  updateProjectAPIKey,
 } from '@/gen/openauth/backend/v1/backend-BackendService_connectquery'
 import { Button } from '@/components/ui/button'
 import { Link } from 'react-router-dom'
+import {
+  DetailsGrid,
+  DetailsGridColumn,
+  DetailsGridEntry,
+  DetailsGridKey,
+  DetailsGridValue,
+} from '@/components/details-grid'
+import { z } from 'zod'
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import {
+  AlertDialog,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from '@/components/ui/alert-dialog'
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form'
+import { Input } from '@/components/ui/input'
+import { InputTags } from '@/components/input-tags'
 
 export function OrganizationDetailsTab() {
   const { organizationId } = useParams()
@@ -21,108 +57,260 @@ export function OrganizationDetailsTab() {
     id: organizationId,
   })
   const { data: getProjectResponse } = useQuery(getProject, {})
+  const { data: getOrganizationGoogleHostedDomainsResponse } = useQuery(
+    getOrganizationGoogleHostedDomains,
+    {
+      organizationId,
+    },
+  )
+  const { data: getOrganizationMicrosoftTenantIdsResponse } = useQuery(
+    getOrganizationMicrosoftTenantIDs,
+    {
+      organizationId,
+    },
+  )
 
   return (
-    <Card>
-      <CardHeader className="flex-row justify-between items-center">
-        <div className="flex flex-col space-y-1 5">
-          <CardTitle>Details</CardTitle>
-          <CardDescription>
-            Additional details about your organization. Lorem ipsum dolor.
-          </CardDescription>
-        </div>
-        <Button variant="outline" asChild>
-          <Link to={`/organizations/${organizationId}/edit`}>Edit</Link>
-        </Button>
-      </CardHeader>
-      <CardContent>
-        <div className="grid grid-cols-3 gap-x-2 text-sm">
-          <div className="border-r border-gray-200 pr-8 flex flex-col gap-4">
-            <div>
-              <div className="font-semibold">Override Login Methods</div>
-              <div>
-                {getOrganizationResponse?.organization?.overrideLogInMethods
-                  ? 'Yes'
-                  : 'No'}
-              </div>
-            </div>
+    <div className="space-y-8">
+      <Card>
+        <CardHeader className="flex-row justify-between items-center">
+          <div className="flex flex-col space-y-1 5">
+            <CardTitle>Details</CardTitle>
+            <CardDescription>
+              Additional details about your organization. Lorem ipsum dolor.
+            </CardDescription>
+          </div>
+          <Button variant="outline" asChild>
+            <Link to={`/organizations/${organizationId}/edit`}>Edit</Link>
+          </Button>
+        </CardHeader>
+        <CardContent>
+          <DetailsGrid>
+            <DetailsGridColumn>
+              <DetailsGridEntry>
+                <DetailsGridKey>Override Login Methods</DetailsGridKey>
+                <DetailsGridValue>
+                  {getOrganizationResponse?.organization?.overrideLogInMethods
+                    ? 'Yes'
+                    : 'No'}
+                </DetailsGridValue>
+              </DetailsGridEntry>
 
-            {getProjectResponse?.project?.logInWithGoogleEnabled && (
-              <div>
-                <div className="font-semibold">Log in with Google</div>
-                <div>
-                  {getOrganizationResponse?.organization?.logInWithGoogleEnabled
+              {getProjectResponse?.project?.logInWithGoogleEnabled && (
+                <DetailsGridEntry>
+                  <DetailsGridKey>Log in with Google</DetailsGridKey>
+                  <DetailsGridValue>
+                    {getOrganizationResponse?.organization
+                      ?.logInWithGoogleEnabled
+                      ? 'Enabled'
+                      : 'Disabled'}
+                  </DetailsGridValue>
+                </DetailsGridEntry>
+              )}
+
+              {getProjectResponse?.project?.logInWithMicrosoftEnabled && (
+                <DetailsGridEntry>
+                  <DetailsGridKey>Log in with Microsoft</DetailsGridKey>
+                  <DetailsGridValue>
+                    {getOrganizationResponse?.organization
+                      ?.logInWithMicrosoftEnabled
+                      ? 'Enabled'
+                      : 'Disabled'}
+                  </DetailsGridValue>
+                </DetailsGridEntry>
+              )}
+
+              {getProjectResponse?.project?.logInWithPasswordEnabled && (
+                <DetailsGridEntry>
+                  <DetailsGridKey>Log in with Password</DetailsGridKey>
+                  <DetailsGridValue>
+                    {getOrganizationResponse?.organization
+                      ?.logInWithPasswordEnabled
+                      ? 'Enabled'
+                      : 'Disabled'}
+                  </DetailsGridValue>
+                </DetailsGridEntry>
+              )}
+            </DetailsGridColumn>
+            <DetailsGridColumn>
+              <DetailsGridEntry>
+                <DetailsGridKey>Configuring SAML</DetailsGridKey>
+                <DetailsGridValue>
+                  {getOrganizationResponse?.organization?.samlEnabled
                     ? 'Enabled'
                     : 'Disabled'}
-                </div>
-              </div>
-            )}
-
-            {getProjectResponse?.project?.logInWithMicrosoftEnabled && (
-              <div>
-                <div className="font-semibold">Log in with Microsoft</div>
-                <div>
-                  {getOrganizationResponse?.organization
-                    ?.logInWithMicrosoftEnabled
+                </DetailsGridValue>
+              </DetailsGridEntry>
+              <DetailsGridEntry>
+                <DetailsGridKey>Configuring SCIM</DetailsGridKey>
+                <DetailsGridValue>
+                  {getOrganizationResponse?.organization?.scimEnabled
                     ? 'Enabled'
                     : 'Disabled'}
-                </div>
-              </div>
-            )}
+                </DetailsGridValue>
+              </DetailsGridEntry>
+            </DetailsGridColumn>
+          </DetailsGrid>
+        </CardContent>
+      </Card>
 
-            {getProjectResponse?.project?.logInWithPasswordEnabled && (
-              <div>
-                <div className="font-semibold">Log in with Password</div>
-                <div>
-                  {getOrganizationResponse?.organization
-                    ?.logInWithPasswordEnabled
-                    ? 'Enabled'
-                    : 'Disabled'}
-                </div>
-              </div>
-            )}
-          </div>
-          <div className="border-r border-gray-200 pl-8 pr-8 flex flex-col gap-4">
-            {getProjectResponse?.project?.logInWithGoogleEnabled && (
-              <div>
-                <div className="font-semibold">Google Hosted Domain</div>
-                <div>
-                  {getOrganizationResponse?.organization?.googleHostedDomain ||
-                    '-'}
-                </div>
-              </div>
-            )}
+      {getOrganizationResponse?.organization?.logInWithGoogleEnabled && (
+        <Card>
+          <CardHeader className="flex-row justify-between items-center">
+            <div className="flex flex-col space-y-1 5">
+              <CardTitle>Google configuration</CardTitle>
+              <CardDescription>
+                Settings related to logging into this organization with Google.
+              </CardDescription>
+            </div>
+            <EditGoogleConfigurationButton />
+          </CardHeader>
+          <CardContent>
+            <DetailsGrid>
+              <DetailsGridColumn>
+                <DetailsGridEntry>
+                  <DetailsGridKey>Log in with Google</DetailsGridKey>
+                  <DetailsGridValue>Enabled</DetailsGridValue>
+                </DetailsGridEntry>
+              </DetailsGridColumn>
+              <DetailsGridColumn>
+                <DetailsGridEntry>
+                  <DetailsGridKey>Google Hosted Domains</DetailsGridKey>
+                  <DetailsGridValue>
+                    {getOrganizationGoogleHostedDomainsResponse
+                      ?.organizationGoogleHostedDomains?.googleHostedDomains
+                      ? getOrganizationGoogleHostedDomainsResponse.organizationGoogleHostedDomains.googleHostedDomains.map(
+                          (s) => <div key={s}>{s}</div>,
+                        )
+                      : '-'}
+                  </DetailsGridValue>
+                </DetailsGridEntry>
+              </DetailsGridColumn>
+            </DetailsGrid>
+          </CardContent>
+        </Card>
+      )}
 
-            {getProjectResponse?.project?.logInWithMicrosoftEnabled && (
-              <div>
-                <div className="font-semibold">Microsoft Tenant ID</div>
-                <div>
-                  {getOrganizationResponse?.organization?.microsoftTenantId ||
-                    '-'}
-                </div>
-              </div>
-            )}
-          </div>
-          <div className="border-gray-200 pl-8 flex flex-col gap-4">
-            <div>
-              <div className="font-semibold">Configuring SAML</div>
-              <div>
-                {getOrganizationResponse?.organization?.samlEnabled
-                  ? 'Enabled'
-                  : 'Disabled'}
-              </div>
-            </div>
-            <div>
-              <div className="font-semibold">Configuring SCIM</div>
-              <div>
-                {getOrganizationResponse?.organization?.scimEnabled
-                  ? 'Enabled'
-                  : 'Disabled'}
-              </div>
-            </div>
-          </div>
-        </div>
-      </CardContent>
-    </Card>
+      {getOrganizationResponse?.organization?.logInWithMicrosoftEnabled && (
+        <Card>
+          <CardHeader>
+            <CardTitle>Microsoft Configuration</CardTitle>
+            <CardDescription>
+              Settings related to logging into this organization with Microsoft.
+            </CardDescription>
+          </CardHeader>
+          <CardContent>
+            <DetailsGrid>
+              <DetailsGridColumn>
+                <DetailsGridEntry>
+                  <DetailsGridKey>Log in with Microsoft</DetailsGridKey>
+                  <DetailsGridValue>Enabled</DetailsGridValue>
+                </DetailsGridEntry>
+              </DetailsGridColumn>
+              <DetailsGridColumn>
+                <DetailsGridEntry>
+                  <DetailsGridKey>Microsoft Tenant IDs</DetailsGridKey>
+                  <DetailsGridValue>
+                    {getOrganizationMicrosoftTenantIdsResponse
+                      ?.organizationMicrosoftTenantIds?.microsoftTenantIds
+                      ? getOrganizationMicrosoftTenantIdsResponse.organizationMicrosoftTenantIds.microsoftTenantIds.map(
+                          (s) => <div key={s}>{s}</div>,
+                        )
+                      : '-'}
+                  </DetailsGridValue>
+                </DetailsGridEntry>
+              </DetailsGridColumn>
+            </DetailsGrid>
+          </CardContent>
+        </Card>
+      )}
+    </div>
+  )
+}
+
+const schema = z.object({
+  googleHostedDomains: z.array(z.string()),
+})
+
+function EditGoogleConfigurationButton() {
+  const { organizationId } = useParams()
+  const { data: getOrganizationGoogleHostedDomainsResponse, refetch } =
+    useQuery(getOrganizationGoogleHostedDomains, {
+      organizationId,
+    })
+  const updateOrganizationGoogleHostedDomainsMutation = useMutation(
+    updateOrganizationGoogleHostedDomains,
+  )
+  const form = useForm<z.infer<typeof schema>>({
+    resolver: zodResolver(schema),
+    defaultValues: {
+      googleHostedDomains: [],
+    },
+  })
+  useEffect(() => {
+    if (
+      getOrganizationGoogleHostedDomainsResponse?.organizationGoogleHostedDomains
+    ) {
+      form.reset({
+        googleHostedDomains:
+          getOrganizationGoogleHostedDomainsResponse
+            .organizationGoogleHostedDomains.googleHostedDomains,
+      })
+    }
+  }, [getOrganizationGoogleHostedDomainsResponse])
+
+  const [open, setOpen] = useState(false)
+
+  async function handleSubmit(values: z.infer<typeof schema>) {
+    await updateOrganizationGoogleHostedDomainsMutation.mutateAsync({
+      organizationId,
+      organizationGoogleHostedDomains: {
+        googleHostedDomains: values.googleHostedDomains,
+      },
+    })
+    await refetch()
+    setOpen(false)
+  }
+
+  return (
+    <AlertDialog open={open} onOpenChange={setOpen}>
+      <AlertDialogTrigger>
+        <Button variant="outline">Edit</Button>
+      </AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>Edit Google configuration</AlertDialogTitle>
+          <AlertDialogDescription>
+            Edit organization google configuration.
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(handleSubmit)}>
+            <FormField
+              control={form.control}
+              name="googleHostedDomains"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Google Hosted Domains</FormLabel>
+                  <FormControl>
+                    <InputTags className="max-w-96" {...field} />
+                  </FormControl>
+                  <FormDescription>
+                    The set of Google workspaces associated with this
+                    organization. Google identifies workspaces by their "hosted
+                    domains", e.g. "example.com".
+                  </FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <AlertDialogFooter className="mt-8">
+              <AlertDialogCancel>Cancel</AlertDialogCancel>
+              <Button type="submit">Save</Button>
+            </AlertDialogFooter>
+          </form>
+        </Form>
+      </AlertDialogContent>
+    </AlertDialog>
   )
 }
