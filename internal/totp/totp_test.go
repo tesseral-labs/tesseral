f600eb8f989259da0c33312f684fad7900b23aba
Ulysse Carion <ulysse.carion@ssoready.com>
2025-01-30 15:11:40 -0800

    Add guts of totp implementation (#187)
    Validated against
    https://datatracker.ietf.org/doc/html/rfc6238#appendix-B

---
+++ internal/totp/totp_test.go
@@ -0,0 +1,34 @@
+package totp_test
+
+import (
+	"fmt"
+	"testing"
+	"time"
+
+	"github.com/openauth/openauth/internal/totp"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestKey_Validate(t *testing.T) {
+	// https://datatracker.ietf.org/doc/html/rfc6238#appendix-B
+	k := totp.Key{Secret: []byte("12345678901234567890")}
+
+	testCases := []struct {
+		unix int64
+		code string
+	}{
+		{59, "287082"},
+		{1111111109, "081804"},
+		{1111111111, "050471"},
+		{1234567890, "005924"},
+		{2000000000, "279037"},
+		{20000000000, "353130"},
+	}
+
+	for _, tt := range testCases {
+		t.Run(fmt.Sprintf("%d", tt.unix), func(t *testing.T) {
+			assert.NoError(t, k.Validate(time.Unix(tt.unix, 0), tt.code))
+			assert.Error(t, k.Validate(time.Unix(tt.unix, 0), "000000"))
+		})
+	}
+}
