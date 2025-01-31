6591acf2f633c2d8e62a7d4489732d5dda82fcf8
Ulysse Carion <ulysse.carion@ssoready.com>
2025-01-31 11:08:24 -0800

    format migration

--- cmd/openauthctl/migrations/000040_passkeys.up.sql
+++ cmd/openauthctl/migrations/000040_passkeys.up.sql
@@ -1,17 +1,17 @@
 create table passkeys
 (
-    id              uuid                     not null primary key,
-    user_id         uuid                     not null references users (id),
-    create_time     timestamp with time zone not null default now(),
-    update_time     timestamp with time zone not null default now(),
-    credential_id   bytea                    not null,
-    public_key bytea                  not null,
-    aaguid          varchar                  not null
+    id            uuid                     not null primary key,
+    user_id       uuid                     not null references users (id),
+    create_time   timestamp with time zone not null default now(),
+    update_time   timestamp with time zone not null default now(),
+    credential_id bytea                    not null,
+    public_key    bytea                    not null,
+    aaguid        varchar                  not null
 );
 
 alter table intermediate_sessions
-    add column passkey_credential_id bytea,
-    add column passkey_public_key bytea,
-    add column passkey_aaguid varchar,
+    add column passkey_credential_id           bytea,
+    add column passkey_public_key              bytea,
+    add column passkey_aaguid                  varchar,
     add column passkey_verify_challenge_sha256 bytea,
-    add column passkey_verified bool not null default false;
+    add column passkey_verified                bool not null default false;
