d8fb058be1029244e6156b1f70b3d1503df4eed9
Ulysse Carion <ulysse.carion@ssoready.com>
2025-01-31 10:00:55 -0800

    fix passkey user id

--- internal/intermediate/store/passkeys.go
+++ internal/intermediate/store/passkeys.go
@@ -7,8 +7,6 @@ import (
 	"github.com/openauth/openauth/internal/common/apierror"
 	"github.com/openauth/openauth/internal/intermediate/authn"
 	intermediatev1 "github.com/openauth/openauth/internal/intermediate/gen/openauth/intermediate/v1"
-	"github.com/openauth/openauth/internal/intermediate/store/queries"
-	"github.com/openauth/openauth/internal/store/idformat"
 )
 
 func (s *Store) GetPasskeyOptions(ctx context.Context, req *intermediatev1.GetPasskeyOptionsRequest) (*intermediatev1.GetPasskeyOptionsResponse, error) {
@@ -32,17 +30,9 @@ func (s *Store) GetPasskeyOptions(ctx context.Context, req *intermediatev1.GetPa
 		return nil, fmt.Errorf("get intermediate session by id: %w", err)
 	}
 
-	qUser, err := q.GetOrganizationUserByEmail(ctx, queries.GetOrganizationUserByEmailParams{
-		OrganizationID: *qIntermediateSession.OrganizationID,
-		Email:          *qIntermediateSession.Email,
-	})
-	if err != nil {
-		return nil, fmt.Errorf("get organization user by email: %w", err)
-	}
-
 	return &intermediatev1.GetPasskeyOptionsResponse{
 		RpId:            *qProject.AuthDomain,
-		UserId:          idformat.User.Format(qUser.ID),
-		UserDisplayName: qUser.Email,
+		UserId:          fmt.Sprintf("%s|%s", *qIntermediateSession.Email, *qIntermediateSession.OrganizationID),
+		UserDisplayName: *qIntermediateSession.Email,
 	}, nil
 }
