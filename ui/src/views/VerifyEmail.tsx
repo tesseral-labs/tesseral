f0da87b48c99d77d84f51b297fc56b5abc00d1bc
Blake Williams <blake.williams@ssoready.com>
2025-02-03 14:17:37 -0800

    Simplify Email verification view

--- ui/src/views/VerifyEmail.tsx
+++ ui/src/views/VerifyEmail.tsx
@@ -91,9 +91,6 @@ const VerifyEmail: FC<VerifyEmailProps> = ({ setView }) => {
                 <InputOTPSlot index={0} />
                 <InputOTPSlot index={1} />
                 <InputOTPSlot index={2} />
-              </InputOTPGroup>
-              <InputOTPSeparator />
-              <InputOTPGroup>
                 <InputOTPSlot index={3} />
                 <InputOTPSlot index={4} />
                 <InputOTPSlot index={5} />
