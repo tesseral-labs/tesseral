990dc150617f77c8b9378d4af9085fc4339be446
Ulysse Carion <ulysse.carion@ssoready.com>
2025-02-22 18:38:10 -0800

    fix config piping

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -172,6 +172,7 @@ func main() {
 		MicrosoftOAuthClientSecretsKMSKeyID:   config.MicrosoftOAuthClientSecretsKMSKeyID,
 		UserContentBaseUrl:                    config.UserContentBaseUrl,
 		AuthAppsRootDomain:                    config.AuthAppsRootDomain,
+		TesseralDNSCloudflareZoneID:           config.TesseralDNSCloudflareZoneID,
 		Cloudflare:                            cloudflare.NewClient(option.WithAPIToken(config.CloudflareAPIToken)),
 		CloudflareDOH:                         &cloudflaredoh.Client{HTTPClient: &http.Client{}},
 	})
