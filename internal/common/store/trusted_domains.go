9d8c45ea6f77dc23655e729126b96e31cb4aef8c
Ulysse Carion <ulysse.carion@ssoready.com>
2025-03-10 11:57:51 -0700

    Trust both http and https origins when trusted domain is localhost

--- internal/common/store/trusted_domains.go
+++ internal/common/store/trusted_domains.go
@@ -3,6 +3,7 @@ package store
 import (
 	"context"
 	"fmt"
+	"net/url"
 
 	"github.com/google/uuid"
 )
@@ -15,7 +16,16 @@ func (s *Store) GetProjectTrustedOrigins(ctx context.Context, projectID uuid.UUI
 
 	var origins []string
 	for _, domain := range domains {
-		origins = append(origins, fmt.Sprintf("https://%s", domain))
+		// Trusted domains are all trusted origins, where the origin scheme is
+		// https. As a special case, if an origin has the hostname "localhost",
+		// then its HTTP (not S) variant is also a trusted origin.
+		origin := url.URL{Scheme: "https", Host: domain}
+		if origin.Hostname() == "localhost" {
+			originHTTP := url.URL{Scheme: "originHTTP", Host: domain}
+			origins = append(origins, originHTTP.String())
+		}
+
+		origins = append(origins, origin.String())
 	}
 
 	return origins, nil
