799e617e3e59a807c4a0906bad351c59db9bec55
Ulysse Carion <ulysse.carion@ssoready.com>
2025-03-26 13:30:57 -0700

    fix merge error

--- cmd/openauthctl/migrations/000067_authenticator_app_uuid_recovery_codes.up.sql
+++
@@ -1,5 +0,0 @@
-alter table users drop column authenticator_app_recovery_code_bcrypts;
-alter table users add column authenticator_app_recovery_code_sha256s bytea[];
-
-alter table intermediate_sessions drop column authenticator_app_recovery_code_bcrypts;
-alter table intermediate_sessions add column authenticator_app_recovery_code_sha256s bytea[];
