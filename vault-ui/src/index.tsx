86b49da115d1619ca11a86f1e92a937a0044726d
Ulysse Carion <ulysse.carion@ssoready.com>
2025-04-04 12:08:26 -0700

    fmt

--- vault-ui/src/index.tsx
+++ vault-ui/src/index.tsx
@@ -1,10 +1,11 @@
+import * as Sentry from "@sentry/react";
 import React from "react";
 import { createRoot } from "react-dom/client";
+
 import { App } from "./App";
-import * as Sentry from "@sentry/react";
 
 Sentry.init({
-  dsn: "https://79f15f3f4f93544077e1e70509ebbdd7@o4505847296557056.ingest.us.sentry.io/4509096519073792"
+  dsn: "https://79f15f3f4f93544077e1e70509ebbdd7@o4505847296557056.ingest.us.sentry.io/4509096519073792",
 });
 
 const root = createRoot(document.getElementById("react-root") as HTMLElement);
