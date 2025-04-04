b1b20c2b7f22b6a513e8ce509b0d9358bc0d03e3
Ulysse Carion <ulysse.carion@ssoready.com>
2025-04-04 11:52:21 -0700

    Add sentry to console, vault

--- vault-ui/src/index.tsx
+++ vault-ui/src/index.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-
 import { App } from "./App";
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn: "https://79f15f3f4f93544077e1e70509ebbdd7@o4505847296557056.ingest.us.sentry.io/4509096519073792"
+});
 
 const root = createRoot(document.getElementById("react-root") as HTMLElement);
 root.render(<App />);
