c0300c8faae4146f2b559b3d4ab9acc17421f692
Blake Williams <blake.williams@ssoready.com>
2025-04-09 08:43:56 -0700

    Remove header from dropdown

--- vault-ui/src/pages/dashboard/DashboardSidebar.tsx
+++ vault-ui/src/pages/dashboard/DashboardSidebar.tsx
@@ -178,26 +178,6 @@ export function DashboardSidebar() {
                 align="end"
                 sideOffset={4}
               >
-                <DropdownMenuLabel className="p-0 font-normal">
-                  <div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
-                    <Avatar className="h-8 w-8 rounded-lg">
-                      <AvatarFallback className="rounded-lg">
-                        {whoamiResponse?.user?.email
-                          ?.substring(0, 1)
-                          ?.toUpperCase()}
-                      </AvatarFallback>
-                    </Avatar>
-                    <div className="grid flex-1 text-left text-sm leading-tight">
-                      <span className="truncate font-semibold">
-                        {whoamiResponse?.user?.email}
-                      </span>
-                      <span className="truncate text-xs">
-                        {whoamiResponse?.user?.email}
-                      </span>
-                    </div>
-                  </div>
-                </DropdownMenuLabel>
-                <DropdownMenuSeparator />
                 <DropdownMenuItem onClick={handleLogout}>
                   <LogOutIcon />
                   Log out
