ee2006c6fdc434e05810aa458000fcff5e26e5ac
Blake Williams <blakethewilliams@gmail.com>
2025-04-15 10:07:41 -0700

    [OPEN-346] Create Users and Organizations from the console (#376)
    This PR adds support for creating Organizations and Users from the
    console. Given the restrictions of the types of Organizations that can
    be associated with the Dogfood Project, there is also a check added to
    the `CreateOrganization` function in the backend store to prevent this
    RPC from being run on the Dogfood Project.
    
    ![Screenshot 2025-04-14 at 12 47
    52 PM](https://github.com/user-attachments/assets/bf04ad1f-43a3-4f87-848c-6ca5a10b0ffc)
    ![Screenshot 2025-04-14 at 12 47
    59 PM](https://github.com/user-attachments/assets/5cc02af7-fbeb-4743-a3a4-add21ebaa4f2)
    ![Screenshot 2025-04-14 at 12 48
    10 PM](https://github.com/user-attachments/assets/5443dbb4-cf1c-45ad-aefe-b2a45778e5fc)
    ![Screenshot 2025-04-14 at 12 48
    15 PM](https://github.com/user-attachments/assets/9809e358-7039-4b46-b174-6ba4ad365238)

--- console/src/pages/organizations/OrganizationUsersTab.tsx
+++ console/src/pages/organizations/OrganizationUsersTab.tsx
@@ -1,6 +1,10 @@
-import React from 'react';
-import { useQuery } from '@connectrpc/connect-query';
-import { listUsers } from '@/gen/tesseral/backend/v1/backend-BackendService_connectquery';
+import React, { FC, useState } from 'react';
+import { useMutation, useQuery } from '@connectrpc/connect-query';
+import {
+  createUser,
+  getOrganization,
+  listUsers,
+} from '@/gen/tesseral/backend/v1/backend-BackendService_connectquery';
 import {
   Card,
   CardContent,
@@ -19,8 +23,33 @@ import {
 import { Link } from 'react-router-dom';
 import { DateTime } from 'luxon';
 import { timestampDate } from '@bufbuild/protobuf/wkt';
-import { useParams } from 'react-router';
+import { useNavigate, useParams } from 'react-router';
 import { Badge } from '@/components/ui/badge';
+import {
+  AlertDialog,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from '@/components/ui/alert-dialog';
+import { Button } from '@/components/ui/button';
+import {
+  Form,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+} from '@/components/ui/form';
+import { z } from 'zod';
+import { useForm } from 'react-hook-form';
+import { AlertDialogCancel } from '@radix-ui/react-alert-dialog';
+import { Input } from '@/components/ui/input';
+import { Switch } from '@/components/ui/switch';
+import { CirclePlus } from 'lucide-react';
+import { toast } from 'sonner';
+import { User } from '@/gen/tesseral/backend/v1/models_pb';
 
 export const OrganizationUsersTab = () => {
   const { organizationId } = useParams();
@@ -30,11 +59,14 @@ export const OrganizationUsersTab = () => {
 
   return (
     <Card>
-      <CardHeader>
-        <CardTitle>Users</CardTitle>
-        <CardDescription>
-          A user is what people using your product log into.
-        </CardDescription>
+      <CardHeader className="flex-row justify-between items-center space-x-4">
+        <div className="flex flex-col space-y-1 5">
+          <CardTitle>Users</CardTitle>
+          <CardDescription>
+            A user is what people using your product log into.
+          </CardDescription>
+        </div>
+        <CreateUserButton />
       </CardHeader>
       <CardContent>
         <Table>
@@ -82,5 +114,179 @@ export const OrganizationUsersTab = () => {
         </Table>
       </CardContent>
     </Card>
+  );
+};
+
+const schema = z.object({
+  email: z.string().email(),
+  googleUserId: z.string().optional(),
+  microsoftUserId: z.string().optional(),
+  owner: z.boolean(),
+});
+
+const CreateUserButton: FC = () => {
+  const navigate = useNavigate();
+
+  const { organizationId } = useParams();
+  const [open, setOpen] = useState(false);
+
+  const { data: organizationResponse } = useQuery(getOrganization, {
+    id: organizationId,
+  });
+
+  const form = useForm<z.infer<typeof schema>>({
+    defaultValues: {
+      email: '',
+      googleUserId: '',
+      microsoftUserId: '',
+      owner: false,
+    },
+  });
+
+  const createUserMutation = useMutation(createUser);
+
+  const handleSubmit = async (data: z.infer<typeof schema>) => {
+    try {
+      const newUser: Partial<User> = {
+        organizationId: organizationId as string,
+        email: data.email,
+        owner: data.owner,
+      };
+
+      if (data.googleUserId) {
+        newUser.googleUserId = data.googleUserId;
+      }
+
+      if (data.microsoftUserId) {
+        newUser.microsoftUserId = data.microsoftUserId;
+      }
+
+      const createUserResponse = await createUserMutation.mutateAsync({
+        user: newUser as User,
+      });
+
+      setOpen(false);
+
+      navigate(
+        `/organizations/${organizationId}/users/${createUserResponse.user?.id}`,
+      );
+
+      toast.success(`User created successfully!`);
+    } catch (error) {
+      console.error('Error creating user:', error);
+    }
+  };
+
+  return (
+    <AlertDialog open={open} onOpenChange={setOpen}>
+      <AlertDialogTrigger asChild>
+        <Button variant="outline">
+          <CirclePlus />
+          Create User
+        </Button>
+      </AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>Create User</AlertDialogTitle>
+          <AlertDialogDescription>
+            Create a new User in the{' '}
+            <span className="text-semibold">
+              {organizationResponse?.organization?.displayName}
+            </span>{' '}
+            Organization.
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <Form {...form}>
+          <form
+            className="space-y-4"
+            onSubmit={form.handleSubmit(handleSubmit)}
+          >
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email</FormLabel>
+                  <Input
+                    type="email"
+                    placeholder="jane.doe@example.com"
+                    {...field}
+                  />
+                  <FormDescription>
+                    The email address of the User being created.
+                  </FormDescription>
+                </FormItem>
+              )}
+            />
+
+            {organizationResponse?.organization?.logInWithGoogle && (
+              <FormField
+                control={form.control}
+                name="googleUserId"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>
+                      Google User ID{' '}
+                      <span className="font-normal text-sm">(optional)</span>
+                    </FormLabel>
+                    <Input placeholder="Google User ID" {...field} />
+                    <FormDescription>
+                      The Google User ID belonging to the User. This is
+                      optional, and will be set on the User automatically on a
+                      successful login attempt.
+                    </FormDescription>
+                  </FormItem>
+                )}
+              />
+            )}
+
+            {organizationResponse?.organization?.logInWithMicrosoft && (
+              <FormField
+                control={form.control}
+                name="microsoftUserId"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>
+                      Microsoft User ID{' '}
+                      <span className="font-normal text-sm">(optional)</span>
+                    </FormLabel>
+                    <Input placeholder="Microsoft User ID" {...field} />
+                    <FormDescription>
+                      The Microsoft User ID belonging to the User. This is
+                      optional, and will be set on the User automatically on a
+                      successful login attempt.
+                    </FormDescription>
+                  </FormItem>
+                )}
+              />
+            )}
+
+            <FormField
+              control={form.control}
+              name="owner"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Owner</FormLabel>
+                  <Switch
+                    className="block"
+                    checked={field.value}
+                    onCheckedChange={field.onChange}
+                  />
+                  <FormDescription>
+                    Whether the User should be an owner of the organization.
+                    This will give them full access to the organization and all
+                    its resources.
+                  </FormDescription>
+                </FormItem>
+              )}
+            />
+            <AlertDialogFooter className="mt-8">
+              <AlertDialogCancel>Cancel</AlertDialogCancel>
+              <Button type="submit">Create</Button>
+            </AlertDialogFooter>
+          </form>
+        </Form>
+      </AlertDialogContent>
+    </AlertDialog>
   );
 };
