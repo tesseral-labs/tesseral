3e9face732d290adcd5ddfcce2a9431ce272fd94
Blake Williams <blakethewilliams@gmail.com>
2025-04-15 10:08:00 -0700

    [OPEN-356] Edit Organizations and Users (#377)
    This PR adds support for editing Users and Organizations from the
    console.
    
    ![Screenshot 2025-04-14 at 1 55
    11 PM](https://github.com/user-attachments/assets/470bbc6a-78c5-43ab-b579-66f89de1ff3a)
    ![Screenshot 2025-04-14 at 1 55
    18 PM](https://github.com/user-attachments/assets/fe00dd07-f484-458f-a13b-5bde4a38b6a0)
    ![Screenshot 2025-04-14 at 2 09
    31 PM](https://github.com/user-attachments/assets/ff53c739-de72-40c5-b2c9-2dcd8ff0b8f0)
    ![Screenshot 2025-04-14 at 2 09
    36 PM](https://github.com/user-attachments/assets/5e59af53-566b-4058-b3eb-b1523df56407)

--- console/src/pages/users/ViewUserPage.tsx
+++ console/src/pages/users/ViewUserPage.tsx
@@ -7,8 +7,9 @@ import {
   getUser,
   listPasskeys,
   listSessions,
+  updateUser,
 } from '@/gen/tesseral/backend/v1/backend-BackendService_connectquery';
-import React from 'react';
+import React, { FC, useEffect, useState } from 'react';
 import {
   Table,
   TableBody,
@@ -48,13 +49,36 @@ import {
 import { DateTime } from 'luxon';
 import { timestampDate } from '@bufbuild/protobuf/wkt';
 import { Button } from '@/components/ui/button';
+import {
+  AlertDialog,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from '@/components/ui/alert-dialog';
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+} from '@/components/ui/form';
+import { useForm } from 'react-hook-form';
+import { z } from 'zod';
+import { Input } from '@/components/ui/input';
+import { Switch } from '@/components/ui/switch';
+import { toast } from 'sonner';
+import { User } from '@/gen/tesseral/backend/v1/models_pb';
 
 export const ViewUserPage = () => {
   const { organizationId, userId } = useParams();
   const { data: getOrganizationResponse } = useQuery(getOrganization, {
     id: organizationId,
   });
-  const { data: getUserResponse } = useQuery(getUser, {
+  const { data: getUserResponse, refetch } = useQuery(getUser, {
     id: userId,
   });
   const { data: listSessionsResponse } = useQuery(listSessions, {
@@ -107,9 +131,12 @@ export const ViewUserPage = () => {
       </PageDescription>
 
       <Card className="my-8">
-        <CardHeader>
-          <CardTitle>General settings</CardTitle>
-          <CardDescription>Basic settings for this user.</CardDescription>
+        <CardHeader className="flex flex-row items-center justify-between space-y-4">
+          <div>
+            <CardTitle>General settings</CardTitle>
+            <CardDescription>Basic settings for this User.</CardDescription>
+          </div>
+          <EditUserSettingsButton />
         </CardHeader>
         <CardContent>
           <DetailsGrid>
@@ -300,7 +327,7 @@ const DangerZoneCard = () => {
           <div>
             <div className="text-sm font-semibold">Impersonate User</div>
             <p className="text-sm">
-              Impersonate this user. You will be logged in as this user. You can
+              Impersonate this User. You will be logged in as this User. You can
               end the impersonated session by logging out.
             </p>
           </div>
@@ -311,5 +338,157 @@ const DangerZoneCard = () => {
         </div>
       </CardContent>
     </Card>
+  );
+};
+
+const schema = z.object({
+  email: z.string().email(),
+  owner: z.boolean(),
+  googleUserId: z.string().optional(),
+  microsoftUserId: z.string().optional(),
+});
+
+const EditUserSettingsButton: FC = () => {
+  const { userId } = useParams();
+  const form = useForm<z.infer<typeof schema>>({
+    defaultValues: {
+      email: '',
+      owner: false,
+      googleUserId: '',
+      microsoftUserId: '',
+    },
+  });
+  const { data: getUserResponse, refetch } = useQuery(getUser, {
+    id: userId,
+  });
+  const updateUserMutation = useMutation(updateUser);
+
+  const [open, setOpen] = useState(false);
+
+  useEffect(() => {
+    if (getUserResponse?.user) {
+      form.reset({
+        email: getUserResponse.user.email,
+        owner: getUserResponse.user.owner,
+        googleUserId: getUserResponse.user.googleUserId || '',
+        microsoftUserId: getUserResponse.user.microsoftUserId || '',
+      });
+    }
+  }, [getUserResponse]);
+
+  const handleSubmit = async (data: z.infer<typeof schema>) => {
+    const updatedUser: Partial<User> = {
+      email: data.email,
+      owner: data.owner,
+    };
+
+    if (data.googleUserId) {
+      updatedUser.googleUserId = data.googleUserId;
+    }
+    if (data.microsoftUserId) {
+      updatedUser.microsoftUserId = data.microsoftUserId;
+    }
+
+    await updateUserMutation.mutateAsync({
+      id: userId,
+      user: updatedUser as User,
+    });
+
+    await refetch();
+
+    setOpen(false);
+    toast.success('User settings updated successfully.');
+  };
+
+  return (
+    <AlertDialog open={open} onOpenChange={setOpen}>
+      <AlertDialogTrigger asChild>
+        <Button variant="outline">Edit</Button>
+      </AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>Edit User Settings</AlertDialogTitle>
+        </AlertDialogHeader>
+        <Form {...form}>
+          <form
+            className="space-y-8"
+            onSubmit={form.handleSubmit(handleSubmit)}
+          >
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email</FormLabel>
+                  <FormControl>
+                    <Input
+                      type="email"
+                      placeholder="jane.doe@example.com"
+                      {...field}
+                    />
+                  </FormControl>
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="googleUserId"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Google User ID</FormLabel>
+                  <FormControl>
+                    <Input
+                      type="text"
+                      placeholder="Google User ID"
+                      {...field}
+                    />
+                  </FormControl>
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="microsoftUserId"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Microsoft User ID</FormLabel>
+                  <FormControl>
+                    <Input
+                      type="text"
+                      placeholder="Microsoft User ID"
+                      {...field}
+                    />
+                  </FormControl>
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="owner"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Owner</FormLabel>
+                  <FormControl>
+                    <Switch
+                      className="block"
+                      checked={field.value}
+                      onCheckedChange={field.onChange}
+                    />
+                  </FormControl>
+                </FormItem>
+              )}
+            />
+
+            <AlertDialogFooter>
+              <AlertDialogCancel>Cancel</AlertDialogCancel>
+              <Button type="submit">Save</Button>
+            </AlertDialogFooter>
+          </form>
+        </Form>
+      </AlertDialogContent>
+    </AlertDialog>
   );
 };
