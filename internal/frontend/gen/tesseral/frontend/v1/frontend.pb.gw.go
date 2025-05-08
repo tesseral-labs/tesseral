214f26e3aaabe7f85735929d444efe786cd58810
Ulysse Carion <ulysse.carion@ssoready.com>
2025-05-08 11:05:22 -0700

    Frontend API + Vault UI

--- internal/frontend/gen/tesseral/frontend/v1/frontend.pb.gw.go
+++ internal/frontend/gen/tesseral/frontend/v1/frontend.pb.gw.go
@@ -1067,12 +1067,20 @@ func local_request_FrontendService_GetUserInvite_0(ctx context.Context, marshale
 	return msg, metadata, err
 }
 
+var filter_FrontendService_CreateUserInvite_0 = &utilities.DoubleArray{Encoding: map[string]int{"user_invite": 0}, Base: []int{1, 1, 0}, Check: []int{0, 1, 2}}
+
 func request_FrontendService_CreateUserInvite_0(ctx context.Context, marshaler runtime.Marshaler, client FrontendServiceClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
 	var (
 		protoReq CreateUserInviteRequest
 		metadata runtime.ServerMetadata
 	)
 	if err := marshaler.NewDecoder(req.Body).Decode(&protoReq.UserInvite); err != nil && !errors.Is(err, io.EOF) {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+	if err := req.ParseForm(); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+	if err := runtime.PopulateQueryParameters(&protoReq, req.Form, filter_FrontendService_CreateUserInvite_0); err != nil {
 		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
 	}
 	msg, err := client.CreateUserInvite(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
@@ -1085,6 +1093,12 @@ func local_request_FrontendService_CreateUserInvite_0(ctx context.Context, marsh
 		metadata runtime.ServerMetadata
 	)
 	if err := marshaler.NewDecoder(req.Body).Decode(&protoReq.UserInvite); err != nil && !errors.Is(err, io.EOF) {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+	if err := req.ParseForm(); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+	if err := runtime.PopulateQueryParameters(&protoReq, req.Form, filter_FrontendService_CreateUserInvite_0); err != nil {
 		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
 	}
 	msg, err := server.CreateUserInvite(ctx, &protoReq)
