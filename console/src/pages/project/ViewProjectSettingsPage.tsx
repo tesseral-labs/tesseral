249030f99734b7cf24b1208f6005e57d6e949f1f
Dillon Nys <24740863+dnys1@users.noreply.github.com>
2025-06-03 08:57:57 -0700

    Update project display name in console (#440)
    Allow users to update the project display name in the console's Project
    Settings page.
     
    <img width="1822" alt="Screenshot 2025-06-02 at 1 30 38 PM"
    src="https://github.com/user-attachments/assets/8fa9c2ce-611b-4960-849e-818f7a9bd4b1"
    />
    
    <img width="1822" alt="Screenshot 2025-06-02 at 1 30 41 PM"
    src="https://github.com/user-attachments/assets/f0aca4e5-c577-4b01-9d7d-e88418d0c2a4"
    />

--- console/src/pages/project/ViewProjectSettingsPage.tsx
+++ console/src/pages/project/ViewProjectSettingsPage.tsx
@@ -1,6 +1,9 @@
-import { useQuery } from '@connectrpc/connect-query';
-import { getProject } from '@/gen/tesseral/backend/v1/backend-BackendService_connectquery';
-import React from 'react';
+import { useMutation, useQuery } from '@connectrpc/connect-query';
+import {
+  getProject,
+  updateProject,
+} from '@/gen/tesseral/backend/v1/backend-BackendService_connectquery';
+import React, { useEffect, useState } from 'react';
 import { DateTime } from 'luxon';
 import { timestampDate } from '@bufbuild/protobuf/wkt';
 import {
@@ -27,6 +30,31 @@ import {
 import { Outlet, useLocation } from 'react-router';
 import { TabBar, TabBarLink } from '@/components/ui/tab-bar';
 import { Settings2 } from 'lucide-react';
+import { z } from 'zod';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { toast } from 'sonner';
+import {
+  AlertDialog,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from '@/components/ui/alert-dialog';
+import {
+  Form,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormDescription,
+  FormControl,
+  FormMessage,
+} from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+import { Button } from '@/components/ui/button';
 
 export const ViewProjectSettingsPage = () => {
   const { data: getProjectResponse } = useQuery(getProject, {});
@@ -90,6 +118,7 @@ export const ViewProjectSettingsPage = () => {
             <ConsoleCardDetails>
               <ConsoleCardTitle>General configuration</ConsoleCardTitle>
             </ConsoleCardDetails>
+            <EditButton />
           </ConsoleCardHeader>
 
           <ConsoleCardContent>
@@ -133,5 +162,77 @@ export const ViewProjectSettingsPage = () => {
         </div>
       </PageContent>
     </>
+  );
+};
+
+const schema = z.object({
+  displayName: z.string(),
+});
+
+const EditButton = () => {
+  const form = useForm<z.infer<typeof schema>>({
+    resolver: zodResolver(schema),
+  });
+
+  const { data: getProjectResponse, refetch } = useQuery(getProject);
+  useEffect(() => {
+    if (getProjectResponse?.project) {
+      form.reset({
+        displayName: getProjectResponse.project.displayName,
+      });
+    }
+  }, [getProjectResponse]);
+
+  const updateProjectMutation = useMutation(updateProject);
+  const [open, setOpen] = useState(false);
+  const handleSubmit = async (values: z.infer<typeof schema>) => {
+    await updateProjectMutation.mutateAsync({
+      project: {
+        displayName: values.displayName,
+      },
+    });
+    await refetch();
+    toast.success('Project updated');
+    setOpen(false);
+  };
+
+  return (
+    <AlertDialog open={open} onOpenChange={setOpen}>
+      <AlertDialogTrigger>
+        <Button variant="outline">Edit</Button>
+      </AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>Edit Project</AlertDialogTitle>
+        </AlertDialogHeader>
+        <Form {...form}>
+          <form
+            className="space-y-4"
+            onSubmit={form.handleSubmit(handleSubmit)}
+          >
+            <FormField
+              control={form.control}
+              name="displayName"
+              render={({ field }: { field: any }) => (
+                <FormItem>
+                  <FormLabel>Display name</FormLabel>
+                  <FormDescription>
+                    A user-facing, human-readable display name for your project.
+                  </FormDescription>
+                  <FormControl>
+                    <Input {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <AlertDialogFooter className="mt-8">
+              <AlertDialogCancel>Cancel</AlertDialogCancel>
+              <Button type="submit">Save</Button>
+            </AlertDialogFooter>
+          </form>
+        </Form>
+      </AlertDialogContent>
+    </AlertDialog>
   );
 };
