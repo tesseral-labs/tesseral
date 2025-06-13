050ef81a49b6b0ccc6779aed04e49dd1157a5321
Blake Williams <blake.williams@ssoready.com>
2025-06-12 17:20:28 -0700

    Add logs for authenticator apps

--- internal/frontend/store/authenticator_apps.go
+++ internal/frontend/store/authenticator_apps.go
@@ -107,13 +107,24 @@ func (s *Store) RegisterAuthenticatorApp(ctx context.Context, req *frontendv1.Re
 		return nil, fmt.Errorf("delete user authenticator app challenge: %w", err)
 	}
 
-	if _, err := q.UpdateUserAuthenticatorApp(ctx, queries.UpdateUserAuthenticatorAppParams{
+	qUser, err := q.UpdateUserAuthenticatorApp(ctx, queries.UpdateUserAuthenticatorAppParams{
 		ID:                                  authn.UserID(ctx),
 		AuthenticatorAppSecretCiphertext:    qUserAuthenticatorAppChallenge.AuthenticatorAppSecretCiphertext,
 		AuthenticatorAppRecoveryCodeSha256s: recoveryCodeSHA256s,
-	}); err != nil {
+	})
+	if err != nil {
 		return nil, fmt.Errorf("update user authenticator app: %w", err)
 	}
+
+	s.logAuditEvent(ctx, q, logAuditEventParams{
+		EventName: "tesseral.users.register_authenticator_app",
+		EventDetails: &frontendv1.UserAuthenticatorAppRegistered{
+			User: parseUser(qUser),
+		},
+		OrganizationID: &qUser.OrganizationID,
+		ResourceType:   queries.AuditLogEventResourceTypeUser,
+		ResourceID:     &qUser.ID,
+	})
 
 	if err := commit(); err != nil {
 		return nil, fmt.Errorf("commit: %w", err)
