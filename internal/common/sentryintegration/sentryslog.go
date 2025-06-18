073d90f19b3117be0b8c684873aaa5afed5b5b66
Dillon Nys <dillon.andre.nys@gmail.com>
2025-06-18 16:16:47 -0700

    Add Sentry slog integration
    Configures an slog handler for mirroring log records as Sentry breadcrumbs. If a Sentry event is triggered (either by panic or by calling sentry.CaptureException), all breadcrumbs up to that point will be attached to the event. The logic in sentryhttp and opaqueinternalerror ensure that every HTTP request has a unique context such that breadcrumbs are scoped to an HTTP request.

---
+++ internal/common/sentryintegration/sentryslog.go
@@ -0,0 +1,113 @@
+package sentryintegration
+
+import (
+	"context"
+	"log/slog"
+	"runtime"
+	"strings"
+
+	"github.com/getsentry/sentry-go"
+)
+
+type sentryHandler struct {
+	base slog.Handler
+}
+
+var _ slog.Handler = (*sentryHandler)(nil)
+
+// NewSlogHandler wraps another slog handle, creating breadcrumbs in Sentry for each log record.
+//
+// Sentry breadcrumbs are a way to record events leading up to an error or crash, which can help in debugging issues.
+func NewSlogHandler(base slog.Handler) slog.Handler {
+	return &sentryHandler{base: base}
+}
+
+func sentryLevel(level slog.Level) sentry.Level {
+	levels := strings.Split(level.String(), "+")
+	return sentry.Level(strings.ToLower(levels[0]))
+}
+
+func attrValue(value slog.Value) any {
+	switch value.Kind() {
+	case slog.KindGroup:
+		group := value.Group()
+		groupAttrs := make(map[string]any, len(group))
+		for _, a := range group {
+			groupAttrs[a.Key] = attrValue(a.Value)
+		}
+		return groupAttrs
+	case slog.KindLogValuer:
+		return attrValue(value.Resolve())
+	default:
+		anyValue := value.Any()
+		switch v := anyValue.(type) {
+		case error:
+			return v.Error() // Convert error to string
+		case []byte:
+			return string(v) // Convert byte slice to string
+		default:
+			return anyValue
+		}
+	}
+}
+
+// Enabled implements slog.Handler.
+func (s *sentryHandler) Enabled(ctx context.Context, level slog.Level) bool {
+	return s.base.Enabled(ctx, level)
+}
+
+// Handle implements slog.Handler.
+func (s *sentryHandler) Handle(ctx context.Context, record slog.Record) error {
+	attrs := make(map[string]any)
+	record.Attrs(func(a slog.Attr) bool {
+		attrs[a.Key] = attrValue(a.Value)
+		return true
+	})
+	addSource(record, attrs)
+
+	hub := sentry.GetHubFromContext(ctx)
+
+	// Don't add breadcrumbs to the global hub.
+	if hub != nil {
+		hub.AddBreadcrumb(&sentry.Breadcrumb{
+			Type:      "default",
+			Data:      attrs,
+			Category:  "slog",
+			Message:   record.Message,
+			Level:     sentryLevel(record.Level),
+			Timestamp: record.Time,
+		}, nil)
+	}
+
+	return s.base.Handle(ctx, record)
+}
+
+// WithAttrs implements slog.Handler.
+func (s *sentryHandler) WithAttrs(attrs []slog.Attr) slog.Handler {
+	return &sentryHandler{base: s.base.WithAttrs(attrs)}
+}
+
+// WithGroup implements slog.Handler.
+func (s *sentryHandler) WithGroup(name string) slog.Handler {
+	return &sentryHandler{base: s.base.WithGroup(name)}
+}
+
+func addSource(r slog.Record, attrs map[string]any) {
+	fs := runtime.CallersFrames([]uintptr{r.PC})
+	f, _ := fs.Next()
+
+	source := make(map[string]any, 3)
+	if f.File != "" {
+		source["file"] = f.File
+	}
+	if f.Line > 0 {
+		source["line"] = f.Line
+	}
+	if f.Function != "" {
+		source["function"] = f.Function
+	}
+	if len(source) == 0 {
+		return
+	}
+	attrs["source"] = source
+}
