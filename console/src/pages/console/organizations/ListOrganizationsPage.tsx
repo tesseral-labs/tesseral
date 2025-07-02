29d80d2757a50008d136acceed177d871d025b0c
Blake Williams <blake.williams@ssoready.com>
2025-07-02 14:52:24 -0700

    Fix lint

--- console/src/pages/console/organizations/ListOrganizationsPage.tsx
+++ console/src/pages/console/organizations/ListOrganizationsPage.tsx
@@ -34,7 +34,6 @@ import {
   AlertDialogFooter,
   AlertDialogHeader,
   AlertDialogTitle,
-  AlertDialogTrigger,
 } from "@/components/ui/alert-dialog";
 import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
