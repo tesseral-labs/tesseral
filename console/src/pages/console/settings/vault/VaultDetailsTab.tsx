0e7d6ea6e392c80c1ad2168fbd69f94740b8484e
Blake Williams <blake.williams@ssoready.com>
2025-07-07 10:34:33 -0700

    Remove redirect URI settings from console

--- console/src/pages/console/settings/vault/VaultDetailsTab.tsx
+++ console/src/pages/console/settings/vault/VaultDetailsTab.tsx
@@ -1,12 +1,10 @@
 import React from "react";
 
 import { VaultBehaviorSettingsCard } from "./details/VaultBehaviorSettingsCard";
-import { VaultRedirectSettingsCard } from "./details/VaultRedirectSettingsCard";
 
 export function VaultDetailsTab() {
   return (
     <div className="grid grid-cols-1 lg:grid-cols-3 gap-8">
-      <VaultRedirectSettingsCard />
       <VaultBehaviorSettingsCard />
     </div>
   );
