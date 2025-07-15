19280c3b3bd10d3a0b73edd3365ef306fd97ce42
Dillon Nys <24740863+dnys1@users.noreply.github.com>
2025-07-15 11:36:58 -0700

    Always output logs to stdout (#549)
    Even when OTEL logging is enabled, we want to ensure that logs are
    always output to stdout.

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -53,6 +53,7 @@ import (
 	intermediatestore "github.com/tesseral-labs/tesseral/internal/intermediate/store"
 	"github.com/tesseral-labs/tesseral/internal/loadenv"
 	"github.com/tesseral-labs/tesseral/internal/microsoftoauth"
+	"github.com/tesseral-labs/tesseral/internal/multislog"
 	oidcinterceptor "github.com/tesseral-labs/tesseral/internal/oidc/authn/interceptor"
 	oidcservice "github.com/tesseral-labs/tesseral/internal/oidc/service"
 	oidcstore "github.com/tesseral-labs/tesseral/internal/oidc/store"
@@ -89,10 +90,9 @@ func main() {
 		panic(fmt.Errorf("init sentry: %w", err))
 	}
 
+	jsonHandler := slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{AddSource: true})
 	slogHandler := ctxlog.NewHandler(
-		sentryintegration.NewSlogHandler(
-			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{AddSource: true}),
-		),
+		sentryintegration.NewSlogHandler(jsonHandler),
 	)
 	slog.SetDefault(slog.New(slogHandler))
 
@@ -188,7 +188,7 @@ func main() {
 
 		slogHandler := ctxlog.NewHandler(
 			sentryintegration.NewSlogHandler(
-				otelslog.NewHandler("api"),
+				multislog.Handler{jsonHandler, otelslog.NewHandler("api")},
 			),
 		)
 		slog.SetDefault(slog.New(slogHandler))
