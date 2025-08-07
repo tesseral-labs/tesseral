7af7e5a25fc70150a742c6a66704c2f18550e9af
Ulysse Carion <ulysse.carion@ssoready.com>
2025-08-07 09:26:19 -0700

    appease linter

--- internal/backgroundworker/store/store.go
+++ internal/backgroundworker/store/store.go
@@ -1,11 +1,8 @@
 package store
 
 import (
-	"context"
-	"fmt"
 	"net/http"
 
-	"github.com/jackc/pgx/v5"
 	"github.com/jackc/pgx/v5/pgxpool"
 	svix "github.com/svix/svix-webhooks/go"
 	"github.com/tesseral-labs/tesseral/internal/backgroundworker/store/queries"
@@ -19,15 +16,4 @@ type Store struct {
 
 func (s *Store) q() *queries.Queries {
 	return queries.New(s.DB)
-}
-
-func (s *Store) tx(ctx context.Context) (tx pgx.Tx, q *queries.Queries, commit func() error, rollback func() error, err error) {
-	tx, err = s.DB.BeginTx(ctx, pgx.TxOptions{})
-	if err != nil {
-		return nil, nil, nil, nil, fmt.Errorf("begin tx: %w", err)
-	}
-
-	commit = func() error { return tx.Commit(ctx) }
-	rollback = func() error { return tx.Rollback(ctx) }
-	return tx, queries.New(tx), commit, rollback, nil
 }
